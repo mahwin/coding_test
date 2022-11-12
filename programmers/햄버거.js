eb1af72068291c7d2a3f878c4980d1d328806837
youseock <mahwin7085@gmail.com>
2022-11-13 04:24:53 +0900

    햄버거

--- programmers/햄버거.js
+++ programmers/햄버거.js
@@ -1,17 +1,27 @@
 function solution(ingredient) {
-  var answer = 0;
-  let stack;
-  ingredient.map((food, idx) => {
-    if (food === 1) {
-      stack = 1;
-    } else if (food === 2 && stack === 1) {
-      stack = 2;
-    } else if (food === 3 && stack === 2) {
-      stack = 0;
-      answer += 1;
-    } else {
-      stack = 0;
+  let answer = 0;
+  let arr = [];
+  ingredient.forEach((food) => {
+    arr.push(food);
+    if (arr.length >= 4) {
+      [completeHambucs, arr] = packingHambuck(arr);
+      answer += completeHambucs;
     }
   });
-  return answer;
+  return console.log(answer);
 }
+
+function packingHambuck(arr) {
+  let completeHambucs = 0;
+  while (true) {
+    const len = arr.length;
+    const endIngredient = arr.slice(len - 4, len);
+    if (endIngredient.join("") === "1231") {
+      completeHambucs += 1;
+      arr.splice(len - 4);
+    } else break;
+  }
+  return [completeHambucs, arr];
+}
+
+solution([2, 1, 1, 2, 3, 1, 2, 3, 1]);
