9cb04797bbda811fc7d0dfeb65b9b21ab0bba5d0
youseock <mahwin7085@gmail.com>
2022-12-09 00:06:30 +0900

    카카오 테트리스

---
+++ programmers/카카오테트리스.js
@@ -0,0 +1,69 @@
+function solution(board) {
+  let answer = 0;
+
+  let tetrisObj = {};
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      // row 0으로
+      // col 0 ~ 끝까지
+      let value = board[row][col];
+      tetrisObj[value] = tetrisObj[value]
+        ? tetrisObj[value].concat([[row, col]])
+        : [[row, col]];
+    }
+  }
+
+  delete tetrisObj[0];
+
+  const validator = (arr, board) => {
+    //마지막과 직전 원소의 행 값이 같아야함.
+    let needCheck = [];
+
+    if (arr[2][0] === arr[3][0]) {
+      //가로로 긴 상태
+      needCheck = arr.filter(([_, col]) => arr[0][1] !== col);
+
+      for (let [row, col] of needCheck) {
+        for (let i = 0; i < row; i++) {
+          if (board[i][col] !== 0) {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+  };
+
+  let flag = true;
+  while (flag) {
+    flag = false;
+    Object.keys(tetrisObj).forEach((key) => {
+      if (validator(tetrisObj[key], board)) {
+        flag = true;
+        tetrisObj[key].forEach(([row, col]) => {
+          board[row][col] = 0;
+        });
+        delete tetrisObj[[key]];
+
+        answer++;
+      }
+    });
+  }
+
+  return answer;
+}
+
+console.log(
+  solution([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
+    [0, 0, 0, 0, 0, 4, 4, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 4, 0, 0, 0],
+    [0, 0, 0, 2, 3, 0, 0, 0, 5, 5],
+    [1, 2, 2, 2, 3, 3, 0, 0, 0, 5],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 5],
+  ])
+);
