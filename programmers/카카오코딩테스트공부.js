c61a3d69485aee3495a5aec22f59bed5a5bfb97c
youseock <mahwin7085@gmail.com>
2022-12-13 00:05:41 +0900

    dp 2022 카카오 코딩 테스트 공부

---
+++ programmers/카카오코딩테스트공부.js
@@ -0,0 +1,49 @@
+function solution(alp, cop, problems) {
+  let maxAlp = alp;
+  let maxCop = cop;
+
+  problems.forEach((problem) => {
+    maxAlp = Math.max(problem[0], maxAlp);
+    maxCop = Math.max(problem[1], maxCop);
+  });
+
+  let dp = Array.from({ length: maxAlp + 1 }, () =>
+    Array.from({ length: maxCop + 1 }, () => Infinity)
+  );
+
+  dp[alp][cop] = 0;
+
+  for (let i = alp; i <= maxAlp; i++) {
+    //알고력
+
+    for (let j = cop; j <= maxCop; j++) {
+      //공부
+      if (i < maxAlp) {
+        dp[i + 1][j] =
+          dp[i][j] + 1 > dp[i + 1][j] ? dp[i + 1][j] : dp[i][j] + 1;
+      }
+      if (j < maxCop) {
+        dp[i][j + 1] =
+          dp[i][j + 1] > dp[i][j] + 1 ? dp[i][j] + 1 : dp[i][j + 1];
+      }
+
+      //문제이용
+
+      for (const [needA, needC, plusA, plusC, cost] of problems) {
+        if (needA <= i && needC <= j) {
+          const a = Math.min(i + plusA, maxAlp);
+          const c = Math.min(j + plusC, maxCop);
+
+          dp[a][c] = dp[i][j] + cost > dp[a][c] ? dp[a][c] : dp[i][j] + cost;
+        }
+      }
+    }
+  }
+
+  return dp[maxAlp][maxCop];
+}
+
+solution(10, 10, [
+  [10, 15, 2, 1, 2],
+  [20, 20, 3, 3, 4],
+]);
