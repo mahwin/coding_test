891728e0f73a44b7b3a55065bf8941f274556731
youseock <mahwin7085@gmail.com>
2022-12-17 21:13:50 +0900

    카카오 불량 사용자 dfs

--- programmers/불량사용자.js
+++ programmers/불량사용자.js
@@ -1,45 +1,43 @@
-const isBanned = (userId, banId) => {
-  if (userId.length !== banId.length) return false;
-  for (let i = 0; i < userId.length; i++) {
-    if (banId[i] !== "*" && banId[i] !== userId[i]) return false;
-  }
-  return true;
-};
+function solution(user_id, banned_id) {
+  const isBanned = (name, ban) => {
+    if (name.length !== ban.length) return false;
+    for (let i = 0; i < name.length; i++) {
+      if (ban[i] === "*") continue;
 
-const solution = (user_id, banned_id) => {
-  const visited = Array(user_id.length).fill(false);
-  const bannedIdSet = new Set();
+      if (ban[i] !== name[i]) return false;
+    }
+    return true;
+  };
 
-  const dfs = (idx, cnt, banString) => {
-    if (cnt === banned_id.length) {
-      let arr = banString.split(" ");
+  let arr = Array.from({ length: banned_id.length }, () => []);
 
-      arr.shift();
-      arr.sort();
-      let str = arr.join("");
-      bannedIdSet.add(str);
+  user_id.forEach((name) => {
+    banned_id.forEach((ban, banIdx) => {
+      if (isBanned(name, ban)) arr[banIdx].push(name);
+    });
+  });
+
+  let banSet = new Set();
+  // const visited = Array.from({ length: arr.length }, () => false);
+  const dfs = (target, tmp) => {
+    if (target === tmp.length) {
+      banSet.add([...tmp].sort().join(""));
+      return;
     }
 
-    for (let i = idx; i < banned_id.length; i++) {
-      for (let j = 0; j < user_id.length; j++) {
-        if (visited[j]) continue;
-        if (!isBanned(user_id[j], banned_id[i])) continue;
-
-        //방문한 적도 없으면서 사용 불가능한 아이디임
-
-        visited[j] = true;
-        dfs(i + 1, cnt + 1, banString + " " + user_id[j]);
-        visited[j] = false;
-      }
+    for (let next of arr[tmp.length]) {
+      if (tmp.includes(next)) continue;
+      tmp.push(next);
+      dfs(target, tmp);
+      tmp.pop();
     }
   };
-  dfs(0, 0, "");
-  return bannedIdSet.size;
-};
+  dfs(arr.length, []);
 
-console.log(
-  solution(
-    ["frodo", "fradi", "crodo", "abc123", "frodoc"],
-    ["*rodo", "*rodo", "******"]
-  )
+  return banSet.size;
+}
+
+solution(
+  ["frodo", "fradi", "crodo", "abc123", "frodoc"],
+  ["fr*d*", "*rodo", "******", "******"]
 );
