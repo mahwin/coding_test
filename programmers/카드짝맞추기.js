1a36da7e23bcfce4db955b9acb31655f07018c0f
youseock <mahwin7085@gmail.com>
2023-01-02 03:29:04 +0900

    카카오 카드짝 맞추기 bfs + permutation 정확도 30%

--- programmers/카드짝맞추기.js
+++ programmers/카드짝맞추기.js
@@ -1,52 +1,92 @@
-const searchCard_bfs = (sy, sx, ey, ex, board) => {
-  if (sy === ey && sx === ex) return [sy, ex, 1];
+let allRemoved = 1;
+let allCard = {};
+let minCnt = Infinity;
+let dirs = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+];
 
-  const queue = [];
-
-  const table = Array.from({ length: 4 }, () =>
-    Array.from({ length: 4 }, () => 0)
-  );
-
-  const visit = Array.from({ length: 4 }, () =>
-    Array.from({ length: 4 }, () => false)
-  );
-
-  // 상하좌우 이동방향 좌표값
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
-
-  queue.push([sy, sx]);
-  visit[sy][sx] = true;
-
-  while (queue.length) {
-    const [y, x] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      let ny = y + dy[i];
-      let nx = x + dx[i];
-
-      // 경계 값 안에 존재하고 방문한 적이 없다면
-      if (!visit[ny][nx] && -1 < ny && ny < 4 && -1 < nx && nx < 4) {
-        visit[ny][nx] = true;
-
-        table[ny][nx] = table[y][x] + 1;
-
-        if (ny === ey && nx === ex) return [ny, nx, table[ny][nx] + 1];
-
-        queue.push([ny, nx]);
-      }
-
-      [ny, nx] = ctrl_move(y, x, dy[i], dx[i], board);
-
-      if (!visit[ny][nx]) {
-        visit[ny][nx] = true;
-        table[ny][nx] = table[y][x] + 1;
-
-        if (ny === ey && nx === ex) return [ny, nx, table[ny][nx] + 1];
-
-        queue.push([ny, nx]);
+function solution(board, r, c) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const num = board[i][j];
+      if (num) {
+        allRemoved |= 1 << num;
+        allCard[num] = allCard[num]
+          ? [...allCard[num], [i, j, 0]] // row, col ,이동 횟수
+          : [[i, j, 0]];
       }
     }
   }
-  return [sy, sx, Infinity];
-};
+  const bfs = (removed, src, dst) => {
+    const visited = Array.from({ length: 4 }, () =>
+      Array.from({ length: 4 }, () => false)
+    );
+    const queue = [src];
+    while (queue.length) {
+      curr = queue.shift();
+      if (curr[0] === dst[0] && curr[1] === dst[1]) return curr[2];
+
+      for (const dir of dirs) {
+        let nr = curr[0] + dir[0];
+        let nc = curr[1] + dir[1];
+
+        if (nr < 0 || nr > 3 || nc < 0 || nc > 3) continue;
+        if (!visited[nr][nc]) {
+          visited[nr][nc] = true;
+          queue.push([nr, nc, curr[2] + 1]);
+        }
+        for (j = 0; j < 2; j++) {
+          if (removed & (1 << board[nr][nc] === 0)) break;
+          if (
+            nr + dir[0] < 0 ||
+            nr + dir[0] > 3 ||
+            nc + dir[1] < 0 ||
+            nc + dir[1] > 3
+          )
+            break;
+          nr += dir[0];
+          nc += dir[1];
+        }
+        if (!visited[nr][nc]) {
+          visited[nr][nc] = true;
+          queue.push([nr, nc, curr[2] + 1]);
+        }
+      }
+    }
+  };
+
+  const permutate = (cnt, removed, src) => {
+    if (removed === allRemoved) {
+      minCnt = Math.min(minCnt, cnt);
+      return;
+    }
+
+    for (const [num, card] of Object.entries(allCard)) {
+      if (removed & (1 << num)) continue;
+
+      one = bfs(removed, src, card[0]) + bfs(removed, card[0], card[1]) + 2;
+      two = bfs(removed, src, card[1]) + bfs(removed, card[1], card[0]) + 2;
+      permutate(cnt + one, removed | (1 << num), card[1]);
+      permutate(cnt + two, removed | (1 << num), card[0]);
+    }
+  };
+
+  permutate(0, 1, [r, c, 0]);
+
+  console.log(minCnt);
+  return minCnt;
+}
+
+solution(
+  [
+    [1, 0, 0, 3],
+    [2, 0, 0, 0],
+    [0, 0, 0, 2],
+    [3, 0, 1, 0],
+  ],
+  1,
+  0
+);
