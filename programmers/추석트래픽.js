6ea043b88918651b7a980b20b794464e707db4a5
youseock <mahwin7085@gmail.com>
2023-01-03 00:36:41 +0900

    카카오 추석트래픽 단순 구현

--- programmers/추석트래픽.js
+++ programmers/추석트래픽.js
@@ -1,42 +1,34 @@
-const solution = (lines) => {
+function solution(lines) {
+  const formatter = (time) => {
+    const [H, M, S] = time.split(":").map(Number);
+
+    return H * 3600 + M * 60 + S;
+  };
+
+  const calTime = (sec, taken) => {
+    sec *= 1000;
+    taken *= 1000;
+    return [sec - taken + 1, sec + 999];
+  };
+
+  const logInfo = [];
+
+  lines.forEach((line) => {
+    const [_, time, taken] = line.split(" ");
+    const sec = formatter(time);
+    const [start, end] = calTime(sec, taken.slice(0, -1));
+    logInfo.push(["start", start]);
+    logInfo.push(["end", end]);
+  });
+
+  logInfo.sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : -1));
+
   let answer = 0;
-  const arr = [];
-  const logPointArr = [];
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].split(" ");
-    const edSec =
-      Number(line[1].substring(0, 2)) * 3600 +
-      Number(line[1].substring(3, 5)) * 60 +
-      Number(line[1].substring(6, 12));
-    const duration = Number(line[2].substring(0, line[2].length - 1));
-    const stSec = edSec - duration + 0.001;
-    arr.push([stSec, edSec]);
-    logPointArr.push(stSec, edSec);
-  }
-
-  logPointArr.sort();
-  console.log(logPointArr);
-
-  for (let i = 0; i < logPointArr.length; i++) {
-    const beginRange = logPointArr[i];
-    const endRange = logPointArr[i] + 1;
-    let count = 0;
-    for (let j = 0; j < arr.length; j++) {
-      const stPoint = arr[j][0];
-      const edPoint = arr[j][1];
-
-      if (
-        (stPoint >= beginRange && stPoint < endRange) ||
-        (edPoint >= beginRange && edPoint < endRange) ||
-        (stPoint <= beginRange && edPoint >= endRange)
-      ) {
-        count++;
-      }
-    }
-
-    if (count > answer) {
-      answer = count;
-    }
+  let cnt = 0;
+  for (const currentLog of logInfo) {
+    if (currentLog[0] === "start") cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
   }
   return answer;
-};
+}
