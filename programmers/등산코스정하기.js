8b4070b16ab1ce0792a67b49ed00b10ac94b9aca
youseock <mahwin7085@gmail.com>
2023-01-08 22:58:25 +0900

    카카오 등산 코스 정하기 : 다익스트라
    -정확성 통과는 보통의 다익스트라 문제에서 그리디 알고리즘을 적용하기 위해 우선순위 큐를 사용하는데 이 문제는 우선 순위 큐를 사용하면
     시간초과 나옴. 우선순위 큐를 큐로 바꾸니 통과. 우선 순위큐는 logN이고 큐는 1의 시간복잡도를 가져서 그런듯.

--- programmers/등산코스정하기.js
+++ programmers/등산코스정하기.js
@@ -1,58 +1,94 @@
-function solution(n, paths, gates, summits) {
-  const pathInfo = Array.from({ length: n + 1 }, () => []);
-  paths.forEach(([i, j, w]) => {
-    pathInfo[i].push([j, w]);
-    pathInfo[j].push([i, w]);
-  });
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-  const distance = Array.from({ length: n + 1 }, () => Infinity);
-  const pq = [];
-  for (const gate of gates) {
-    pq.push([0, gate]); //[강도, 노드]
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  while (pq.length > 0) {
-    const intensArr = pq.map((el) => el[0]);
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
 
-    const smallIdx = intensArr.indexOf(Math.min(...intensArr));
-    const [intensity, node] = pq[smallIdx];
-    console.log(pq);
-    pq.splice(smallIdx, 1);
-    console.log(pq);
-    if (distance[node] <= intensity) continue;
-    distance[node] = intensity;
+  enqueue(data) {
+    const node = new Node(data);
+    if (!this.head) this.head = node;
+    else this.tail.next = node;
+
+    this.tail = node;
+    this.size++;
+  }
+  dequeue() {
+    if (!this.head) return null;
+    const data = this.head.data;
+    this.head = this.head.next;
+    this.size--;
+    return data;
+  }
+}
+
+function solution(n, paths, gates, summits) {
+  let graph = Array.from({ length: n + 1 }, () => []); //그래프
+  const queue = new Queue(); // 큐
+  const d = Array.from({ length: n + 1 }, () => Infinity); // 다익스크라 값 배열
+
+  //양방향 그래프
+  paths.forEach(([from, to, intensity]) => {
+    graph[from].push([to, intensity]);
+    graph[to].push([from, intensity]);
+  });
+
+  //우선 순위 큐 첫 시작 값
+  for (let i = 0; i < gates.length; i++) {
+    queue.enqueue([gates[i], 0]);
+  }
+
+  while (!queue.isEmpty()) {
+    const [node, intensity] = queue.dequeue();
+
+    if (d[node] <= intensity) continue;
+    d[node] = intensity;
     if (summits.includes(node)) continue;
 
-    for (const [nextNode, nextIntens] of pathInfo[node]) {
-      console.log(intensity, nextIntens);
-      const nextIntensity = Math.max(intensity, nextIntens);
+    for (let [next, nextIntens] of graph[node]) {
+      const nextIntensity = intensity > nextIntens ? intensity : nextIntens;
 
-      if (distance[nextNode] && distance[nextNode] <= nextIntensity) continue;
-      pq.push([nextIntensity, nextNode]);
+      if (d[next] <= nextIntensity) continue;
+      // d[next] = nextIntensity;
+      queue.enqueue([next, nextIntensity]);
     }
   }
   let answer = [0, Infinity];
 
   for (const summit of summits) {
-    if (distance[summit] < answer[1]) {
-      answer = [summit, distance[summit]];
-    } else if (distance[summit] === answer[1] && summit < answer[0]) {
-      answer = [summit, distance[summit]];
+    if (d[summit] < answer[1]) {
+      answer = [summit, d[summit]];
+    } else if (d[summit] === answer[1] && summit < answer[0]) {
+      answer = [summit, d[summit]];
     }
   }
+
   return answer;
 }
 
-n = 7;
-paths = [
-  [1, 4, 4],
-  [1, 6, 1],
-  [1, 7, 3],
-  [2, 5, 2],
-  [3, 7, 4],
-  [5, 6, 6],
-];
-gates = [1, 3, 7];
-summits = [2, 4];
-
-console.log(solution(n, paths, gates, summits));
+solution(
+  6,
+  [
+    [1, 2, 3],
+    [2, 3, 5],
+    [2, 4, 2],
+    [2, 5, 4],
+    [3, 4, 4],
+    [4, 5, 3],
+    [4, 6, 1],
+    [5, 6, 1],
+  ],
+  [1, 3],
+  [5]
+);
