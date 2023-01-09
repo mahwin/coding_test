ae1786266d5e93c785e92495cfba3287adbc1d24
youseock <mahwin7085@gmail.com>
2023-01-09 17:35:31 +0900

    PCCP : 2번 dfs 구현

--- pccp/2.js
+++ pccp/2.js
@@ -1,51 +1,73 @@
-const getCombination = (arr, pick) => {
-  if (pick === 1) return arr.map((el) => [el]);
-  const result = [];
+// const getCombination = (arr, pick) => {
+//   if (pick === 1) return arr.map((el) => [el]);
+//   const result = [];
 
-  arr.forEach((fixed, index) => {
-    const tmp = getCombination(arr.slice(index + 1), pick - 1);
-    tmp.forEach((el) => result.push([fixed, ...el]));
-  });
-  return result;
-};
+//   arr.forEach((fixed, index) => {
+//     const tmp = getCombination(arr.slice(index + 1), pick - 1);
+//     tmp.forEach((el) => result.push([fixed, ...el]));
+//   });
+//   return result;
+// };
 
-const getPermutation = (arr, pick) => {
-  if (pick === 1) return arr.map((el) => [el]);
-  const result = [];
+// const getPermutation = (arr, pick) => {
+//   if (pick === 1) return arr.map((el) => [el]);
+//   const result = [];
 
-  arr.forEach((fixed, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-    const tmp = getPermutation(rest, pick - 1);
-    tmp.forEach((el) => result.push([fixed, ...el]));
-  });
-  return result;
-};
+//   arr.forEach((fixed, index, origin) => {
+//     const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+//     const tmp = getPermutation(rest, pick - 1);
+//     tmp.forEach((el) => result.push([fixed, ...el]));
+//   });
+//   return result;
+// };
+
+// function solution(ability) {
+//   let answer = 0;
+//   let targetNum = ability[0].length;
+//   let peopleNum = ability.length;
+
+//   const peopleCombis = getCombination(
+//     Array.from({ length: peopleNum }, (_, i) => i),
+//     targetNum
+//   );
+
+//   const pickPermus = getPermutation(
+//     Array.from({ length: targetNum }, (_, i) => i),
+//     targetNum
+//   );
+
+//   for (let people of peopleCombis) {
+//     for (let picks of pickPermus) {
+//       let sum = 0;
+//       for (let i = 0; i < picks.length; i++) {
+//         sum += ability[people[i]][picks[i]];
+//       }
+
+//       if (answer < sum) answer = sum;
+//     }
+//   }
+//   return answer;
+// }
 
 function solution(ability) {
-  let answer = 0;
-  let targetNum = ability[0].length;
-  let peopleNum = ability.length;
+  let answer = Number.MIN_SAFE_INTEGER;
+  const peopleNum = ability.length;
+  const kindOfSport = ability[0].length;
+  const visited = Array.from({ length: peopleNum }, () => false);
 
-  const peopleCombis = getCombination(
-    Array.from({ length: peopleNum }, (_, i) => i),
-    targetNum
-  );
-
-  const pickPermus = getPermutation(
-    Array.from({ length: targetNum }, (_, i) => i),
-    targetNum
-  );
-
-  for (let people of peopleCombis) {
-    for (let picks of pickPermus) {
-      let sum = 0;
-      for (let i = 0; i < picks.length; i++) {
-        sum += ability[people[i]][picks[i]];
-      }
-
-      if (answer < sum) answer = sum;
+  const dfs = (start, sum) => {
+    if (start === kindOfSport) {
+      answer = Math.max(answer, sum);
     }
-  }
+    for (let i = 0; i < peopleNum; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        dfs(start + 1, sum + ability[i][start]);
+        visited[i] = false;
+      }
+    }
+  };
+  dfs(0, 0);
   return answer;
 }
 
