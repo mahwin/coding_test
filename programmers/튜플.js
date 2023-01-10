8a46f035886a7fe63fecbb7ae28f22f2ee810bd3
youseock <mahwin7085@gmail.com>
2023-01-10 21:54:45 +0900

    카카오 튜플 : 단순 문자열 다루기 문제

--- programmers/튜플.js
+++ programmers/튜플.js
@@ -1,31 +1,14 @@
 function solution(s) {
-  let answer = [];
-  s = s.slice(1, s.length - 1);
-  let sObj = {};
-  let tmp = "";
-  for (let char of s) {
-    if (char === "{") {
-      tmp = "";
-    } else if (char === "}") {
-      let arr = tmp.split(",");
-      arr.length !== 0 ? (sObj[arr.length] = arr.map(Number)) : null;
-    } else {
-      tmp += char;
-    }
-  }
-  Object.values(sObj).forEach((arr) => {
-    arr.forEach((n) => {
-      !answer.includes(n) ? answer.push(n) : null;
-    });
-  });
-  return answer;
+  let set = new Set();
+
+  s.slice(2, -2)
+    .split("},{")
+    .map((el) => el.split(",").map(Number))
+    .sort((a, b) => a.length - b.length)
+    .forEach((el) => el.forEach((num) => set.add(num)));
+
+  return [...set];
 }
 
-function solution(s) {
-  return s
-    .slice(2, -2)
-    .split("},{")
-    .map((str) => str.split(",").map(Number))
-    .sort((a, b) => a.length - b.length)
-    .reduce((acc, cur) => [...acc, ...cur.filter((n) => !acc.includes(n))], []);
-}
+let s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+console.log(solution(s));
