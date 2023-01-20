e9cdb38d5f100e932b239b5498df5e27a2d090dc
youseock <mahwin7085@gmail.com>
2023-01-20 17:32:32 +0900

    프로그래머스 110 옮기기 단순 구현

--- programmers/110옮기기.js
+++ programmers/110옮기기.js
@@ -4,12 +4,10 @@ function solution(s) {
     let str = s[t];
     let stack = [];
     let tmp = find110(str, stack);
-
     if (tmp == "") answer.push(str);
     else {
       const tmpStr = stack.join("");
       const idx = tmpStr.lastIndexOf("0") + 1;
-      // console.log(idx);
       answer.push(tmpStr.substring(0, idx) + tmp + tmpStr.substring(idx));
     }
   }
@@ -23,15 +21,16 @@ function find110(str, stack) {
     if (stack.length >= 2) {
       const b = stack.pop();
       const a = stack.pop();
-
       if (a == "1" && b == "1" && c == "0") {
         tmp += "110";
         continue;
       }
       stack.push(a);
       stack.push(b);
-    } // if
+    }
     stack.push(c);
   }
   return tmp;
 }
+
+console.log(solution(["1110", "100111100", "0111111010"]));
