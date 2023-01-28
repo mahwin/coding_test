d1df0fecb9afac032e2357f8afdcf07d2647f688
youseock <mahwin7085@gmail.com>
2023-01-28 23:31:20 +0900

    삼성 sw expert 치킨 배달 : 정확성 통과, bfs + 조합

--- samsungSW/치킨배달.js
+++ samsungSW/치킨배달.js
@@ -1,19 +1,22 @@
-let input = `5 3
-0 0 1 0 0
-0 0 2 0 1
-0 1 2 0 0
-0 0 1 0 0
-0 0 0 0 2`.split("\n");
+let input = `5 1
+1 2 0 2 1
+1 2 0 2 1
+1 2 0 2 1
+1 2 0 2 1
+1 2 0 2 1`.split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
 const colLength = input[0].split(" ").length;
-const chickenPos = [];
-const housePos = [];
 
+const housePos = [];
+const chickenPos = [];
+
+const aliveBoard = Array.from({ length: N }, () =>
+  Array.from({ length: colLength }, () => false)
+);
 const chickenBoard = Array.from({ length: N }, () =>
   Array.from({ length: colLength }, () => 0)
 );
-
 for (let i = 0; i < N; i++) {
   let curRow = input[i].split(" ").map(Number);
   for (let j = 0; j < colLength; j++) {
@@ -21,7 +24,10 @@ for (let i = 0; i < N; i++) {
 
     if (!cur) continue;
     if (cur === 1) housePos.push([i, j]);
-    else chickenPos.push([i, j]);
+    else {
+      chickenPos.push([i, j]);
+      chickenBoard[i][j] = 1;
+    }
   }
 }
 
@@ -37,12 +43,13 @@ const isValid = (r, c) => {
   return true;
 };
 
-const findChicken = (row, col, chickenBoard, isAlive) => {
+const findChicken = (row, col, aliveBoard) => {
   let queue = [[row, col, 0]];
+
   while (queue.length) {
     const [r, c, cnt] = queue.shift();
 
-    if (chickenBoard[r][c]) return cnt;
+    if (chickenBoard[r][c] && aliveBoard[r][c]) return cnt;
 
     for (const dir of dirs) {
       const nr = r + dir[0];
@@ -58,7 +65,7 @@ const getCombination = (arr, pick) => {
   if (pick === 1) return arr.map((el) => [el]);
   const result = [];
   arr.forEach((fixed, index) => {
-    const tmp = getCombination(arr.slice(index), pick - 1);
+    const tmp = getCombination(arr.slice(index + 1), pick - 1);
     tmp.forEach((el) => result.push([fixed, ...el]));
   });
   return result;
@@ -67,3 +74,24 @@ const getCombination = (arr, pick) => {
 let arr = Array.from({ length: chickenPos.length }, (_, i) => i);
 
 const combis = getCombination(arr, M);
+let min = Infinity;
+
+for (const com of combis) {
+  let tmp = 0;
+  for (const pos of housePos) {
+    com.forEach((el) => {
+      let [i, j] = chickenPos[el];
+      aliveBoard[i][j] = true;
+    });
+    tmp += findChicken(pos[0], pos[1], aliveBoard);
+  }
+
+  min = Math.min(tmp, min);
+
+  com.forEach((el) => {
+    let [i, j] = chickenPos[el];
+    aliveBoard[i][j] = false;
+  });
+}
+
+console.log(min);
