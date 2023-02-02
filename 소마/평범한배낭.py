46b7eeaeaf79a6632750ef92d4dfe3fe75a56ed0
youseock <mahwin7085@gmail.com>
2023-02-02 16:25:26 +0900

    백준 평범한 배낭 : 0-1 냅색 알고리즘

--- 소마/평범한배낭.py
+++ 소마/평범한배낭.py
@@ -1,20 +1,27 @@
+# 전형적인 knapsack 이차원 dp 배열을 만들어서 해결한다
+# 시간 복잡도 O(N * Weight )
+# 공간 복잡도 O(N * Weight )인데 O(Weight)로 줄일 수 있긴함.
+
 from sys import stdin
-[N,MAX_WEIGHT] = list(map(int,stdin.readline().rstrip().split(' ')))
-dp  = [[0 for _ in range(MAX_WEIGHT+1)] for _ in range(N+1)]
 
-w_v_info =[[0,0]]
+N,W = map(int,stdin.readline().rstrip().split())
+bags=[0]
+vals=[0]
+
 for _ in range(N):
-  w_v_info.append(list(map(int,stdin.readline().rstrip().split(' '))))
+  b,v =map(int,stdin.readline().rstrip().split())
+  bags.append(b)
+  vals.append(v)
 
 
-for i in range(1,N+1):
-  for j in range(1,MAX_WEIGHT+1):
-    w = w_v_info[i][0]
-    v = w_v_info[i][1]
+dp =[[0 for _ in range(W+1)] for _ in range(N+1)]
 
-    if j < w :
-      dp[i][j] = dp[i-1][j]
-    else :
-      dp[i][j] = max(v + dp[i-1][j-w],dp[i-1][j])
+for n in range(N+1):
+  for w in range(W+1):
+    
+    if w-bags[n] >=0:
+      dp[n][w] = max(dp[n-1][w-bags[n]] + vals[n], dp[n-1][w])
+    else:
+      dp[n][w] = dp[n-1][w]
 
-print(dp[N][MAX_WEIGHT])
+print(dp[N][W])
