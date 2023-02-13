e9e80e046584ddcf71f01581ec61b5bd6f6082fb
youseock <mahwin7085@gmail.com>
2023-02-13 23:36:27 +0900

    백준 직사각형 2527 : 구현

--- 소마/직사각형.js
+++ 소마/직사각형.js
@@ -1,7 +1,7 @@
-let input = `2 2 3 3 3 3 4 4   
-2 2 3 3 1 1 2 2
-2 2 3 3 3 1 4 2
-2 2 3 3 1 3 2 4
+let input = `3 10 50 60 100 100 200 300
+45 50 600 600 400 450 500 543
+11 120 120 230 50 40 60 440
+35 56 67 90 67 80 500 600
 `.split("\n");
 
 // let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
@@ -18,21 +18,20 @@ const checkBox = (posArr) => {
 
 for (let i = 0; i < 4; i++) {
   let info = input[i].split(" ").map(Number);
+  solution(info);
+}
+
+function solution(info) {
   let [r1, c1, r2, c2] = info.slice(0, 4);
   let [r3, c3, r4, c4] = info.slice(4);
-
   let arr = [];
-  for (let row = r1; row <= r2; row++) {
-    if (row >= r3 && row <= r4) {
-      for (let col = c1; col <= c2; col++) {
-        if (col >= c3 && col <= c4) {
-          arr.push([row, col]);
-        }
-      }
-    }
-  }
-  if (arr.length === 0) console.log("d");
-  else if (arr.length === 1) console.log("c");
-  else if (checkBox(arr)) console.log("a");
-  else console.log("b");
+  if (r2 < r3 || r1 > r4 || c2 < c3 || c1 > c4) return console.log("d");
+  // b,c의 경우
+  else if (r1 === r4 || r2 === r3) {
+    if (c1 === c4 || c2 === c3) return console.log("c");
+    return console.log("b");
+  } else if (c1 === c4 || c2 === c3) {
+    if (r1 === r4 || r2 === r3) return console.log("c");
+    return console.log("b");
+  } else return console.log("a");
 }
