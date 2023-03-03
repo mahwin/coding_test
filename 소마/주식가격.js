2d1425a8f669a4b7b7c2b5a4ef3a2da2f59601dc
youseock <mahwin7085@gmail.com>
2023-03-03 14:03:30 +0900

    프로그래머스 주식가격 : stack

--- 소마/주식가격.js
+++ 소마/주식가격.js
@@ -1,34 +1,56 @@
-function solution(prices) {
-  let stack = [[prices[0], 0]];
+// function solution(prices) {
+//   let stack = [[prices[0], 0]];
+//   let result = Array.from({ length: prices.length }, () => 0);
+//   for (let i = 1; i < prices.length; i++) {
+//     let curP = prices[i];
+
+//     if (stack && stack[stack.length - 1][0] > curP) {
+//       let deteleArr = [];
+
+//       stack.forEach(([p, i], k) => {
+//         if (p > curP) deteleArr.push(k);
+//       });
+
+//       deteleArr = deteleArr.reverse();
+//       deteleArr.forEach((j) => {
+//         const [i, start] = stack.splice(j, 1)[0];
+//         result[start] = i - start;
+//       });
+//     }
+//     stack.push([curP, i]);
+//   }
+//   const len = prices.length - 1;
+//   if (stack.length) {
+//     stack.forEach(([p, t]) => {
+//       result[t] = len - t;
+//     });
+//   }
+
+//   return result;
+// }
+
+console.log(sol([1, 2, 3, 4, 5, 6, 1, 1, 2, 3, 1, 2, 3]));
+
+function sol(prices) {
+  let stack = [];
   let result = Array.from({ length: prices.length }, () => 0);
-  for (let i = 1; i < prices.length; i++) {
+  for (let i = 0; i < prices.length; i++) {
     let curP = prices[i];
+    if (!stack.length) {
+      stack.push([curP, i]);
+      continue;
+    }
 
-    if (stack && stack[stack.length - 1][0] > curP) {
-      let deteleArr = [];
+    while (stack[stack.length - 1][0] > curP) {
+      [_, buyDate] = stack.pop();
 
-      stack.forEach(([p, i], k) => {
-        if (p > curP) deteleArr.push(k);
-      });
-      console.log(deteleArr, "!!");
-      deteleArr = deteleArr.reverse();
-      deteleArr.forEach((j) => {
-        const [i, start] = stack.splice(j, 1)[0];
-        result[start] = i - start;
-      });
+      result[buyDate] = i - buyDate;
     }
     stack.push([curP, i]);
   }
-  const len = prices.length - 1;
-  if (stack.length) {
-    stack.forEach(([p, t]) => {
-      result[t] = len - t;
-    });
-  }
 
+  stack.forEach(([_, d]) => {
+    result[d] = prices.length - 1 - d;
+  });
   return result;
 }
-
-console.log(solution([1, 2, 3, 2, 3]));
-
-console.log(solution([1, 2, 3, 4, 5, 6, 1, 1, 2, 3, 1, 2, 3]));
