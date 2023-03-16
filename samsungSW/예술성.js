09c150d35a39cd094c6b6fe5588ad0077ba1d320
youseock <mahwin7085@gmail.com>
2023-03-17 00:33:15 +0900

    회전 함수 개선 (모듈화)

--- samsungSW/예술성.js
+++ samsungSW/예술성.js
@@ -1,5 +1,6 @@
 let n;
 let board;
+let copy;
 let result = 0;
 
 const dirs = [
@@ -104,73 +105,81 @@ function solution() {
     result += val;
   }
 }
-function rotate(board) {
-  const copy = Array.from({ length: n }, () =>
-    Array.from({ length: n }, () => 0)
-  );
+function sqare_rotate(sr, sc, sq_length) {
+  for (let r = sr; r < sr + sq_length; r++) {
+    for (let c = sc; c < sc + sq_length; c++) {
+      const [or, oc] = [r - sr, c - sc];
+      const [rr, rc] = [oc, sq_length - or - 1];
 
-  //   십자가
+      copy[sr + rr][sc + rc] = board[r][c];
+    }
+  }
+}
+function rotate(board) {
+  const n = board.length;
+  copy = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+
+  const sq_length = Math.floor(n / 2);
+
+  for (let r = 0; r < n; r++) {
+    copy[sq_length][r] = board[r][sq_length];
+  }
+
+  for (let c = 0; c < n; c++) {
+    copy[n - c - 1][sq_length] = board[sq_length][c];
+  }
+
+  sqare_rotate(0, 0, sq_length);
+  sqare_rotate(sq_length + 1, 0, sq_length);
+  sqare_rotate(0, sq_length + 1, sq_length);
+  sqare_rotate(sq_length + 1, sq_length + 1, sq_length);
+
   for (let r = 0; r < n; r++) {
     for (let c = 0; c < n; c++) {
-      copy[n - c - 1][r] = board[r][c];
+      board[r][c] = copy[r][c];
     }
   }
-  const N = Math.floor(n / 2);
-  //1번 사각형
-  for (let r = 0; r < N; r++) {
-    for (let c = 0; c < N; c++) {
-      copy[c][N - r - 1] = board[r][c];
-    }
-  }
-
-  //2번 사각형
-  for (let r = 0; r < N; r++) {
-    for (let c = N + 1; c < n; c++) {
-      copy[c - N - 1][n - r - 1] = board[r][c];
-    }
-  }
-  //3번 사각형
-  for (let r = N + 1; r < n; r++) {
-    for (let c = 0; c < N; c++) {
-      copy[N + c + 1][N - (r - N)] = board[r][c];
-    }
-  }
-
-  //4번 사각형
-
-  for (let r = N + 1; r < n; r++) {
-    for (let c = N + 1; c < n; c++) {
-      copy[c][n - (r - N)] = board[r][c];
-    }
-  }
-  return copy.map((el) => [...el]);
 }
 
 // console.log(board);
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let input = [];
+// let input = [];
 
-rl.on("line", (line) => {
-  input.push(line);
-});
+// rl.on("line", (line) => {
+//   input.push(line);
+// });
 
-rl.on("close", () => {
-  n = Number(input[0]);
-  board = [];
-  for (let i = 1; i <= n; i++) {
-    board.push(input[i].split(" ").map(Number));
-  }
-  for (let trial = 0; trial < 4; trial++) {
-    board = trial === 0 ? board : rotate(board);
-    solution(board);
-  }
+// rl.on("close", () => {
+//   n = Number(input[0]);
+//   board = [];
 
-  console.log(result);
-  process.exit();
-});
+//   for (let i = 1; i <= n; i++) {
+//     board.push(input[i].split(" ").map(Number));
+//   }
+
+//   for (let trial = 0; trial < 4; trial++) {
+//     board = trial === 0 ? board : rotate(board);
+//     solution(board);
+//   }
+
+//   console.log(result);
+//   process.exit();
+// });
+
+board = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+];
+
+rotate(board);
+
+console.log(board);
