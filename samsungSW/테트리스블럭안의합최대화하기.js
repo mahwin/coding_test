154f2716cf4c83cc43ccb3a9e1b1b592ad65d169
youseock <mahwin7085@gmail.com>
2023-04-15 18:53:53 +0900

    삼성 sw 2017 테트리스 블럭 안의 합 최대화 하기 : 백트래킹

--- samsungSW/테트리스블럭안의합최대화하기.js
+++ samsungSW/테트리스블럭안의합최대화하기.js
@@ -1,50 +1,48 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 let input = [];
+let v; // 방문 체크
 let n, m;
-let root = [];
-let result = 0;
+let max = -Infinity; // 합의 최대 저장
+
 const dirs = [
+  // 네 방향으로 서치
   [0, 1],
   [1, 0],
   [-1, 0],
   [0, -1],
 ];
 
-rl.on("line", (line) => {
-  input.push(line.split(" ").map(Number));
-});
-
-rl.on("close", () => {
+rl.on("line", function (line) {
+  input.push(line.trim().split(" ").map(Number));
+}).on("close", function () {
   solution();
   process.exit();
 });
 
+// 경계 확인
 const isValid = (r, c) => {
-  //경계 안이고
   if (r < 0 || c < 0 || r >= n || c >= m) return false;
-  //방문한적 없으면
-  if (v[r][c]) return false;
   return true;
 };
 
-const findSquare = (root, acc, cnt) => {
+// 테트리스 블럭 모양 찾기.
+const searchBlock = (roots, cnt, acc) => {
   if (cnt === 4) {
-    result = Math.max(result, acc);
+    max = Math.max(max, acc);
     return;
   }
-  for (const [r, c] of root) {
+
+  for (const root of roots) {
     for (const d of dirs) {
-      const nr = d[0] + r;
-      const nc = d[1] + c;
-      if (isValid(nr, nc)) {
+      const nr = root[0] + d[0];
+      const nc = root[1] + d[1];
+      if (isValid(nr, nc) && !v[nr][nc]) {
         v[nr][nc] = true;
-        findSquare([...root, [nr, nc]], acc + input[nr][nc], cnt + 1);
+        searchBlock([...roots, [nr, nc]], cnt + 1, acc + input[nr][nc]);
         v[nr][nc] = false;
       }
     }
@@ -54,13 +52,16 @@ const findSquare = (root, acc, cnt) => {
 const solution = () => {
   [n, m] = input.shift();
   v = Array.from({ length: n }, () => Array.from({ length: m }, () => false));
+
   for (let r = 0; r < n; r++) {
     for (let c = 0; c < m; c++) {
+      console.log(r, c, v);
       v[r][c] = true;
-      findSquare([[r, c]], input[r][c], 1);
-      // v[r][c] =false;
+      searchBlock([[r, c]], 1, input[r][c]); // 이동 경로,이동 횟수, 숫자합
     }
   }
-
-  console.log(result);
+  console.log(max);
 };
+
+// 수행 시간  1185ms
+// 사용 공간 	18MB
