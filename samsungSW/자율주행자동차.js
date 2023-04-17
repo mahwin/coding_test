5d63bd40f8bf2cc4197dbd70e6666d5ea721bad0
youseock <mahwin7085@gmail.com>
2023-04-17 15:38:58 +0900

    삼성 sw 2017 자율 주행 자동차 : simulation

--- samsungSW/자율주행자동차.js
+++ samsungSW/자율주행자동차.js
@@ -1,66 +1,82 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+// 0 북 1 동 2 남 3 서
+const dirs = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+
+let n, m; // rowLen, colLen
+let carPos; // [r,c,d]
+let result = 1; //결과 값
+let v; // 방문 체크
 let input = [];
-rl.on("line", (line) => {
-  input.push(line);
-});
 
-rl.on("close", () => {
-  const [rowLen, colLen] = input[0].split(" ").map(Number);
-
-  const dirs = [
-    [-1, 0],
-    [0, 1],
-    [1, 0],
-    [0, -1],
-  ];
-  //d : 0 북 1 동 2 남 3 서
-  const [row, col, d] = input[1].split(" ").map(Number);
-
-  const board = [];
-
-  for (let i = 2; i < 2 + rowLen; i++) {
-    board.push(input[i].split(" ").map(Number));
-  }
-
-  const v = Array.from({ length: rowLen }, () =>
-    Array.from({ length: colLen }, () => false)
-  );
-  let result = 1;
-  v[row][col] = true;
-
-  const isValid = (r, c) => {
-    if (r < 0 || c < 0 || r >= rowLen || c >= colLen) return false;
-    return true;
-  };
-  const queue = [[row, col, d]];
-  while (queue.length) {
-    let [r, c, d] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      d = d - 1 === -1 ? 3 : d - 1;
-      const nr = dirs[d][0] + r;
-      const nc = dirs[d][1] + c;
-
-      if (isValid(nr, nc) && !v[nr][nc] && !board[nr][nc]) {
-        v[nr][nc] = true;
-        result++;
-        queue.push([nr, nc, d]);
-        break;
-      }
-      if (i === 3) {
-        //4방향 다 가도 갈 곳이 없었을 경우
-        const br = r - dirs[d][0];
-        const bc = c - dirs[d][1];
-        if (!board[br][bc]) {
-          queue.push([br, bc, d]);
-        }
-      }
-    }
-  }
-  console.log(result);
+rl.on("line", function (line) {
+  input.push(line.trim().split(" ").map(Number));
+}).on("close", function () {
+  solution();
   process.exit();
 });
+
+const isValid = (r, c) => {
+  //경계 안에 대한 검증
+  if (r < 0 || c < 0 || r >= n || c >= m) return false;
+  //갈 수 있는 곳인가에 대한 검증
+  if (input[r][c] === 1) return false;
+  return true;
+};
+
+const solution = () => {
+  [n, m] = input[0];
+  carPos = input[1];
+  input = input.slice(2);
+  // 이동할 때 마다
+
+  //방문처리
+  v = Array.from({ length: n }, () => Array.from({ length: m }, () => false));
+
+  v[carPos[0]][carPos[1]] = true;
+
+  drive(...carPos);
+
+  console.log(result);
+};
+
+const drive = (r, c, d) => {
+  //전진
+  for (let i = 0; i < 4; i++) {
+    const nd = d - 1 > -1 ? d - 1 : 3;
+    const nR = r + dirs[nd][0];
+    const nC = c + dirs[nd][1];
+
+    if (isValid(nR, nC)) {
+      if (!v[nR][nC]) {
+        //갈수도 있고, 방문한적 있으면
+
+        v[nR][nC] = true;
+        result++;
+        drive(nR, nC, nd);
+        return;
+      }
+    }
+    // 조건에 부합하는 갈 수 있는 곳이 없다면 방향만 바꾸고 반복문 다시 진행
+    d = nd;
+  }
+
+  //후진
+  const rR = r - dirs[d][0];
+  const rC = c - dirs[d][1];
+  if (isValid(rR, rC)) drive(rR, rC, d);
+};
+
+// 수행 시간 417ms
+// 메모리 10MB
+
+// 경계 값 검증 함수(isValid)에서 계속 row 경계 값이랑 col 경계 값을 반대로 넣거나 같은 값을 넣는 실수를 함...;
