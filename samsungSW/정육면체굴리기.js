1201bf36a9e6b551205871ea59f80359a76cd746
youseock <mahwin7085@gmail.com>
2023-04-20 16:36:01 +0900

    삼성 sw 2016 정육면체 굴리기 : simulation

--- samsungSW/정육면체굴리기.js
+++ samsungSW/정육면체굴리기.js
@@ -6,66 +6,77 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let board = [];
-let dices = Array.from({ length: 7 }, () => 0); //[밑,앞,오른,왼,뒤,위]
-let n, m, x, y, k;
-let [up, front, right] = [1, 2, 3];
+let rowLen, colLen, r, c, k; // 행 길이, 열 길이, 주사위의 row, 주사위의 col, 주사위 굴린 횟수.
+const dices = Array.from({ length: 6 }, () => 0);
+const dirs = [[], [0, 1], [0, -1], [-1, 0], [1, 0]]; // 1 동 2 서 3 북 4 남
+let [front, right, bottom] = [0, 1, 2];
+
 rl.on("line", (line) => {
-  input.push(line.trim());
+  input.push(line.trim().split(" ").map(Number));
 });
 
 rl.on("close", () => {
-  [n, m, x, y, k] = input[0].split(" ").map(Number);
-
-  for (let i = 1; i <= n; i++) {
-    board.push(input[i].split(" ").map(Number));
-  }
-  //1 동 2 서 3 북 4 남
-  const dirs = [null, [0, 1], [0, -1], [-1, 0], [1, 0]];
-  const roots = input[1 + n].split(" ").map(Number);
-
-  roots.forEach((root) => {
-    const d = dirs[root];
-    const nx = d[0] + x;
-    const ny = d[1] + y;
-    if (isValid(nx, ny)) {
-      //굴리는게 가능하다면
-      [x, y] = [nx, ny];
-      moveDice(root, nx, ny);
-      getDiceUp();
-    }
-  });
-
+  simulation();
   process.exit();
 });
 
-const isValid = (x, y) => {
-  if (x < 0 || y < 0 || x >= n || y >= m) return false;
+// 경계 확인
+const isValid = (r, c) => {
+  if (r < 0 || c < 0 || r >= rowLen || c >= colLen) return false;
   return true;
 };
 
-const moveDice = (direction, nx, ny) => {
-  if (direction === 1) {
-    //동
-    [up, front, right] = [7 - right, front, up];
-  } else if (direction === 2) {
-    //서
-    [up, front, right] = [right, front, 7 - up];
-  } else if (direction === 3) {
-    //북
-    [up, front, right] = [front, 7 - up, right];
-  } else if (direction === 4) {
-    //남
-    [up, front, right] = [7 - front, up, right];
-  }
+const simulation = () => {
+  [rowLen, colLen, r, c, k] = input.shift();
+  const ds = input[rowLen];
 
-  if (board[nx][ny] === 0) {
-    board[nx][ny] = dices[7 - up];
-  } else {
-    dices[7 - up] = board[nx][ny];
-    board[nx][ny] = 0;
+  for (const d of ds) {
+    const nr = dirs[d][0] + r;
+    const nc = dirs[d][1] + c;
+    if (isValid(nr, nc)) {
+      rotate(d);
+      paintAndPrint(nr, nc);
+      r = nr;
+      c = nc;
+    }
   }
 };
-const getDiceUp = () => {
-  console.log(dices[up]);
+
+//회전
+const rotate = (d) => {
+  if (d === 1) {
+    // 동쪽으로 돌리면 right => bottom, bottom과 짝을 이루는 제일 위의 상자가 right가 됨
+    let tmp = bottom;
+    bottom = right;
+    right = 5 - tmp;
+  }
+  if (d === 2) {
+    // 서쪽으로 돌리면 bottom => right, right와 짝인 left가 bottom
+    let tmp = bottom;
+    bottom = 5 - right;
+    right = tmp;
+  }
+  if (d === 3) {
+    // 북으로 돌리면 bottom이 front front와 짝인 부분이 bottom
+    let tmp = front;
+    front = bottom;
+    bottom = 5 - tmp;
+  }
+  if (d === 4) {
+    // 남으로 돌리면 front => bottom bottom과 짝인 부분이 front
+    let tmp = bottom;
+    bottom = front;
+    front = 5 - tmp;
+  }
+};
+
+//주사위나 판에 숫자를 페인팅하고 주사위 제일 상단의 숫자를 프린팅.
+const paintAndPrint = (r, c) => {
+  if (input[r][c] === 0) {
+    input[r][c] = dices[bottom];
+  } else {
+    dices[bottom] = input[r][c];
+    input[r][c] = 0;
+  }
+  console.log(dices[5 - bottom]);
 };
