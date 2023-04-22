b284fef332004affa16dd636fcf2618ddee81c7d
youseock <mahwin7085@gmail.com>
2023-04-22 15:18:17 +0900

    삼성 sw 2017 돌아가는 팔각 의자 : simulation

--- samsungSW/돌아가는팔각의자.js
+++ samsungSW/돌아가는팔각의자.js
@@ -1,76 +1,86 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-let input = [];
-rl.on("line", (line) => {
-  input.push(line);
-});
+let chairs = [];
+let rotateArr = [];
+let TOTAL_CHAIR = 4; // 총 팔각의자의 수
 
-rl.on("close", () => {
-  let chairs = [];
-  for (let i = 0; i < 4; i++) {
-    chairs.push(input[i].split("").map(Number));
-  }
-
-  let trials = Number(input[4]);
-
-  for (let i = 5; i < trials + 5; i++) {
-    let [chairIdx, direction] = input[i].split(" ").map(Number);
-
-    chairIdx--; // 의자 번호 0번 부터 시작하도록
-    let rotateDirections = {}; // key는 chairIdx, value는 도는 방향. 정방향 1 역방향 -1
-    rotateDirections[chairIdx] = direction;
-
-    //오른 방향
-    let next = chairIdx + 1;
-    let cur = chairIdx;
-    let d = direction;
-    while (next < 4) {
-      if (chairs[cur][2] !== chairs[next][6]) {
-        d = d === 1 ? -1 : 1;
-        rotateDirections[next] = d;
-        next += 1;
-        cur += 1;
-      } else break;
-    }
-
-    //왼 방향
-    pre = chairIdx - 1;
-    cur = chairIdx;
-    d = direction;
-    while (pre > -1) {
-      if (chairs[pre][2] !== chairs[cur][6]) {
-        d = d === 1 ? -1 : 1;
-        rotateDirections[pre] = d;
-        pre -= 1;
-        cur -= 1;
-      } else break;
-    }
-    cycle(chairs, rotateDirections);
-  }
-  console.log(calScore(chairs));
+rl.on("line", function (line) {
+  chairs.push(line.trim());
+}).on("close", function () {
+  solution();
   process.exit();
 });
 
-const cycle = (arr, rotateObj) => {
-  for (let [idx, d] of Object.entries(rotateObj)) {
+// 특정 의자를 시계 or 반시계 방향으로 돌려줌
+//    시계  [0,1,2,3] => [3,0,1,2]
+// 반 시계  [0,1,2,3] => [1,2,3,0]
+const rotate = () => {
+  for (const [chairIdx, d] of rotateArr) {
     if (d === 1) {
-      let last = arr[idx].pop();
-      arr[idx].unshift(last);
+      //시계 방향
+      const last = chairs[chairIdx].pop();
+      chairs[chairIdx].unshift(last);
     } else {
-      let first = arr[idx].shift();
-      arr[idx].push(first);
+      //반 시계 방향
+      const first = chairs[chairIdx].shift();
+      chairs[chairIdx].push(first);
     }
   }
 };
 
-const calScore = (arr) => {
-  let result = 0;
-  arr.forEach((el, i) => {
-    result += 2 ** i * el[0];
-  });
-  return result;
+const simmulation = (chairIdx, d) => {
+  // 앞의 의자의 경우 2번 인덱스와 현재 의자의 6번 의자의 값이 다르면 2번 의자 돌아감.
+  // 뒤의 의자의 경우 현재 의자 2번 인덱스와 뒤 의자 6번 인덱스의 값이 다르면 의자 돌아감.
+
+  rotateArr = [[chairIdx, d]]; //돌아가는 의자의 인덱스와 방향을 저장해놓고 모두 확인 후 돌림.
+
+  let preIdx = chairIdx - 1;
+  let nextIdx = chairIdx + 1;
+
+  while (preIdx > -1) {
+    if (chairs[preIdx][2] === chairs[preIdx + 1][6]) break;
+
+    let diff = Math.abs(chairIdx - preIdx);
+    rotateArr.push([preIdx, diff % 2 !== 0 ? -1 * d : d]);
+    preIdx -= 1;
+  }
+
+  while (nextIdx < 4) {
+    if (chairs[nextIdx][6] === chairs[nextIdx - 1][2]) break;
+    let diff = Math.abs(chairIdx - nextIdx);
+
+    rotateArr.push([nextIdx, diff % 2 !== 0 ? -1 * d : d]);
+    nextIdx += 1;
+  }
 };
+
+// 각 팔각의자 12시 방향에 몇명의 남쪽 사람이 앉아있는지 카운팅.
+const cntNorthPeopleintwelveClock = () => {
+  let cnt = 0;
+  for (let i = 0; i < 4; i++) {
+    if (chairs[i][0] === 1) cnt += 2 ** i;
+  }
+  console.log(cnt);
+};
+
+const solution = () => {
+  //의자를 배열 형태로 접근하기 위함.
+  for (let i = 0; i < 4; i++) {
+    chairs[i] = chairs[i].split("").map(Number);
+  }
+
+  const n = Number(chairs[TOTAL_CHAIR]);
+  for (let i = TOTAL_CHAIR + 1; i < TOTAL_CHAIR + 1 + n; i++) {
+    const [chairIdx, d] = chairs[i].split(" ").map(Number);
+    simmulation(chairIdx - 1, d); // 0번 부터 의자 시작
+    rotate();
+  }
+
+  cntNorthPeopleintwelveClock();
+};
+
+// 수행 시간 : 413ms
+// 메모리 : 10MB
