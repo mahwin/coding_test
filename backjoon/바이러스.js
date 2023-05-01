1fd2f5f958a7a80d8fd9d7e11d562569ff13bcf8
youseock <mahwin7085@gmail.com>
2023-05-01 17:45:15 +0900

    백준 2606 바이러스 : 그래프 탐색, dfs

---
+++ backjoon/바이러스.js
@@ -0,0 +1,43 @@
+const input = [];
+let graph,
+  visited,
+  result = 0;
+
+const strToNumArr = (str) =>
+  str.split(" ").map((numString) => Number(numString));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const N = Number(input.shift());
+    graph = [...Array(N + 1)].map(() => []);
+    visited = Array(N + 1).fill(false);
+
+    input.splice(0, 1);
+    let v1, v2;
+    input.forEach((str) => {
+      [v1, v2] = strToNumArr(str);
+      graph[v1].push(v2);
+      graph[v2].push(v1);
+    });
+
+    dfs(1);
+    console.log(result - 1);
+  });
+
+const dfs = (v) => {
+  if (visited[v]) {
+    return;
+  }
+
+  visited[v] = true;
+  result++;
+  graph[v].forEach((vertex) => {
+    if (!visited[vertex]) {
+      dfs(vertex);
+    }
+  });
+};
