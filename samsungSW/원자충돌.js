8f7721482fd4ae0de9103c31d6eea30cf0b1eaf8
youseock <mahwin7085@gmail.com>
2023-05-03 00:10:35 +0900

    삼성 sw 2020 원자 충돌 : simulation

--- samsungSW/원자충돌.js
+++ samsungSW/원자충돌.js
@@ -20,8 +20,9 @@ const ewsnDirs = [0, 2, 4, 6]; //동서남북
 const diagonalDirs = [1, 3, 5, 7]; //대각선
 
 let input = [];
-let n, atomNum, k; // n X n 격자, atomNum 원자 수, k 실험 시간.
 let atoms = [];
+let n, m, k;
+let atomMap, dupleMap;
 
 rl.on("line", (line) => {
   input.push(line.trim());
@@ -32,46 +33,61 @@ rl.on("close", () => {
   process.exit();
 });
 
+//원자들의 이동
 const move = () => {
-  const dupleObj = {};
-  const dupleSet = new Set();
-  let newAtoms = [];
+  atomMap = new Map();
+  dupleMap = new Map();
 
   atoms.forEach(([r, c, m, s, d]) => {
-    const nr = (r + dirs[d][0] * s + s * n) % n;
-    const nc = (c + dirs[d][1] * s + s * n) % n;
-
+    const nr = (r + dirs[d][0] * s + n * s) % n;
+    const nc = (c + dirs[d][1] * s + n * s) % n;
     const key = [nr, nc].join(",");
-    if (dupleObj[key]) {
-      dupleSet.add(key);
-      dupleObj[key][0] += m;
-      dupleObj[key][1] += s;
+    if (!dupleMap.has(key) && !atomMap.has(key)) {
+      //최초의 원소
+      atomMap.set(key, [m, s, d]);
+    } else if (atomMap.has(key)) {
+      //아톰에 값이 있으니까 얘는 atom 값은 지우고 중첩으로 올려줌
+      const info = atomMap.get(key);
+      atomMap.delete(key);
+      dupleMap.set(key, [m + info[0], s + info[1], 2, [d, info[2]]]);
     } else {
-      dupleObj[key] = [m, s];
-      newAtoms.push([nr, nc, m, s, d]);
+      const info = dupleMap.get(key);
+      dupleMap.set(key, [
+        m + info[0],
+        s + info[1],
+        info[2] + 1,
+        info[3].concat(d),
+      ]);
     }
   });
-
-  // 겹침이 없었던 위치의 원자는 필터.
   atoms = [];
-  atoms = newAtoms.filter(([r, c, ...info]) => {
-    const key = [r, c].join(",");
-    return dupleSet.has(key) ? false : true;
-  });
-
-  [...dupleSet].forEach((key) => {
+  for (const [key, value] of atomMap.entries()) {
     const [r, c] = key.split(",").map(Number);
-    let [m, s] = dupleObj[key];
-    m = Math.floor(m / 5);
-    s = Math.floor(s/ )
-    if (m > 0) {
-      let nextDirs = canGoESWN(r, c) ? ewsnDirs : diagonalDirs;
-      nextDirs.forEach((nextD) => atoms.push([r, c, m, s, nextD]));
-    }
-  });
+    atoms.push([r, c, ...value]);
+  }
 };
 
-const sumAtom = () => {
+const isENWS = (ds) => {
+  const newDs = ds.filter((el) => ewsnDirs.includes(el));
+  if (newDs.length === ds.length || !newDs.length) return true;
+  return false;
+};
+
+const dupleAtom = () => {
+  for (const [key, value] of dupleMap.entries()) {
+    // value  [무게 합, 속도 합, 몇 개의 원소, 각 원소들의 방향들]
+    const [r, c] = key.split(",").map(Number);
+    const [totalM, totalS, cnt, ds] = value;
+    let eachM = Math.floor(totalM / 5);
+    if (eachM > 0) {
+      let eachS = Math.floor(totalS / cnt);
+      let newD = isENWS(ds) ? ewsnDirs : diagonalDirs;
+      newD.forEach((d) => atoms.push([r, c, eachM, eachS, d]));
+    }
+  }
+};
+
+const cntWeight = () => {
   let result = 0;
   atoms.forEach((info) => {
     result += info[2];
@@ -80,17 +96,17 @@ const sumAtom = () => {
 };
 
 const solution = () => {
-  [n, atomNum, k] = input[0].split(" ").map(Number);
-
-  for (let i = 1; i <= atomNum; i++) {
-    const [r, c, m, s, d] = input[i].split(" ").map(Number);
-    atoms.push([r - 1, c - 1, m, s, d]);
+  [n, m, k] = input[0].split(" ").map(Number);
+  //초기 원소 넣기
+  for (let i = 1; i <= m; i++) {
+    const [r, c, ...remain] = input[i].split(" ").map(Number);
+    atoms.push([r - 1, c - 1, ...remain]);
   }
 
   while (k--) {
     move();
-    console.log(atoms);
+    dupleAtom();
   }
-  console.log(atoms);
-  sumAtom();
+
+  cntWeight();
 };
