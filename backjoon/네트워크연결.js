977f02bb60b66152c9644aefd19d72ac06f6b38f
youseock <mahwin7085@gmail.com>
2023-05-07 15:53:29 +0900

    백준 1922 네트워크 연결 : 크루스칼 알고리즘

--- backjoon/네트워크연결.js
+++ backjoon/네트워크연결.js
@@ -1,53 +1,48 @@
-const getParent = (parent, x) => {
-  if (parent[x] === x) return x;
-  return (parent[x] = getParent(parent, parent[x]));
-};
+const readline = require("readline");
 
-const unionParent = (parent, a, b) => {
-  a = getParent(parent, a);
-  b = getParent(parent, b);
-
-  if (a < b) parent[b] = a;
-  else parent[a] = b;
-};
-
-const findParent = (parent, a, b) => {
-  a = getParent(parent, a);
-  b = getParent(parent, b);
-
-  if (a === b) return true;
-  return false;
-};
-
-let input = `6
-9
-1 2 5
-1 3 4
-2 3 2
-2 4 7
-3 4 6
-3 5 11
-4 5 3
-4 6 8
-5 6 8`.split("\n");
-
-const nodes = +input[0];
-const vertex = +input[1];
-let answer = 0;
-const parent = [];
-for (let i = 1; i < nodes + 1; i++) {
-  parent[i] = i;
-}
-console.log(parent);
-let costs = input.slice(2).map((el) => el.split(" ").map(Number));
-costs.sort((a, b) => a[2] - b[2]);
-console.log(costs);
-
-costs.forEach(([from, to, cost], i) => {
-  if (!findParent(parent, from, to)) {
-    console.log(parent);
-    answer += cost;
-    unionParent(parent, from, to);
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-console.log(answer);
+
+let input = [];
+
+rl.on("line", (line) => {
+  input.push(line.trim());
+});
+
+rl.on("close", () => {
+  const n = +input[0];
+  const m = +input[1];
+  const p = Array.from({ length: n + 1 }, (_, i) => i);
+  let cost = 0;
+
+  input = input
+    .slice(2)
+    .map((el) => el.split(" ").map(Number))
+    .sort((a, b) => a[2] - b[2]);
+
+  const getP = (x) => {
+    if (p[x] === x) return x;
+    return (p[x] = getP(p[x]));
+  };
+
+  const union = (x1, x2, c) => {
+    const p1 = getP(x1);
+    const p2 = getP(x2);
+
+    //사이클이 생기면 비용 합산 x
+    if (p1 === p2) return;
+
+    p1 > p2 ? (p[p1] = p2) : (p[p2] = p1);
+    cost += c;
+  };
+
+  for (let i = 0; i < m; i++) {
+    const [x1, x2, c] = input[i];
+    union(x1, x2, c);
+  }
+
+  console.log(cost);
+  process.exit();
+});
