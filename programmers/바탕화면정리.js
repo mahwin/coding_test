ed0bfaff7ca26ac5bd8a654af26430f6b4fe9d99
youseock <mahwin7085@gmail.com>
2023-05-12 14:47:41 +0900

    프로그래머스 바탕화면 정리 : 구현

--- programmers/바탕화면정리.js
+++ programmers/바탕화면정리.js
@@ -1,53 +1,16 @@
 function solution(wallpaper) {
-  let r1, c1, r2, c2;
-  let flag = false;
-  for (let r = 0; r < wallpaper.length; r++) {
-    for (let c = 0; c < wallpaper[0].length; c++) {
-      if (wallpaper[r][c] == "#") {
-        r1 = r;
-        flag = true;
-        break;
-      }
-    }
-    if (flag) break;
-  }
-  flag = false;
-  for (let r = wallpaper.length - 1; r > -1; r--) {
-    for (let c = 0; c < wallpaper[0].length; c++) {
-      if (wallpaper[r][c] == "#") {
-        r2 = r + 1;
-        flag = true;
-        break;
-      }
-    }
-    if (flag) break;
-  }
-  flag = false;
-  for (let c = 0; c < wallpaper[0].length; c++) {
-    for (let r = 0; r < wallpaper.length; r++) {
-      if (wallpaper[r][c] == "#") {
-        c1 = c;
-        flag = true;
-        break;
-      }
-    }
-    if (flag) break;
-  }
-  flag = false;
-  for (let c = wallpaper[0].length - 1; c > -1; c--) {
-    for (let r = 0; r < wallpaper.length; r++) {
-      console.log(c, r);
-      if (wallpaper[r][c] == "#") {
-        c2 = c + 1;
-        flag = true;
-        break;
-      }
-    }
-    if (flag) break;
-  }
-  if (r1 === r2) r2++;
-  if (c1 === c2) c2++;
-  return [r1, c1, r2, c2];
-}
+  let [minR, minC, maxR, maxC] = [Infinity, Infinity, 0, 0];
 
-console.log(solution(["..", "#."]));
+  wallpaper.forEach((rowInfo, r) => {
+    rowInfo.split("").forEach((el, c) => {
+      if (el === "#") {
+        minR = Math.min(minR, r);
+        minC = Math.min(minC, c);
+        maxR = Math.max(maxR, r);
+        maxC = Math.max(maxC, c);
+      }
+    });
+  });
+
+  return [minR, minC, maxR + 1, maxC + 1];
+}
