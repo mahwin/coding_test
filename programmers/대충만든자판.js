bd7d3a3c1c35a995088c9baca5bb5b685c07b767
youseock <mahwin7085@gmail.com>
2023-05-12 14:25:57 +0900

    프로그래머스 대충만든 자판 : 구현

--- programmers/대충만든자판.js
+++ programmers/대충만든자판.js
@@ -1,35 +1,27 @@
 function solution(keymap, targets) {
-  let alphaCnt = new Map();
   let answer = [];
-  for (let i = 0; i < keymap.length; i++) {
-    const key = keymap[i];
-    for (let j = 0; j < key.length; j++) {
-      const alpha = key[j];
-      if (alphaCnt.has(alpha)) {
-        if (alphaCnt.get(alpha) > j + 1) {
-          alphaCnt.set(alpha, j + 1);
-        }
-      } else {
-        alphaCnt.set(alpha, j + 1);
-      }
-    }
-  }
+
+  const keyobj = {};
+
+  keymap.forEach((keys) => {
+    keys.split("").forEach((alpha, i) => {
+      keyobj[alpha] = keyobj[alpha] ? Math.min(keyobj[alpha], i + 1) : i + 1;
+    });
+  });
 
   for (let i = 0; i < targets.length; i++) {
-    let tmp = 0;
     const target = targets[i];
+    let flag = true;
+    let cnt = 0;
     for (let j = 0; j < target.length; j++) {
-      const alpha = target[j];
-      if (!alphaCnt.has(alpha)) {
+      const key = target[j];
+      if (!keyobj[key]) {
         answer.push(-1);
+        flag = false;
         break;
-      }
-
-      tmp += alphaCnt.get(alpha);
+      } else cnt += keyobj[key];
     }
-    if (answer.length === i + 1) continue;
-    answer.push(tmp);
+    if (flag) answer.push(cnt);
   }
-
   return answer;
 }
