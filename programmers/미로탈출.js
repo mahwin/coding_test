fab316969d3017059d7b43143e43e10776357ab7
youseock <mahwin7085@gmail.com>
2023-05-12 11:11:45 +0900

    프로그래머스 미로탈출 : bfs, 방문 배열 3차원 처리

--- programmers/미로탈출.js
+++ programmers/미로탈출.js
@@ -1,83 +1,52 @@
-const d = Array.from({ length: 1024 }, () =>
-  Array.from({ length: 1004 }, () => Infinity)
-);
+function solution(maps) {
+  let answer = 0;
+  const rowLen = maps.length;
+  const colLen = maps[0].length;
 
-const adj = Array.from({ length: 1004 }, () => []);
-const adjRev = Array.from({ length: 1004 }, () => []);
-const trapIdx = Array.from({ length: 1004 }, () => -1);
+  const dirs = [
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [0, -1],
+  ]; // 이동 방향
 
-const bitmask = (state, idx) => {
-  return (1 << trapIdx[idx]) & state;
-};
+  const isValid = (r, c) => {
+    //경계 값 확인
+    if (r < 0 || c < 0 || r >= rowLen || c >= colLen) return false;
+    return true;
+  };
 
-function solution(n, start, end, roads, traps) {
-  roads.forEach(([from, to, cost]) => {
-    adj[from].push([to, cost]);
-    adjRev[to].push([from, cost]);
-  });
+  //방문체크 row,col,레버 작동
+  const v = Array.from({ length: rowLen }, () =>
+    Array.from({ length: colLen }, () => Array.from({ length: 2 }, () => false))
+  );
 
-  for (let i = 0; i < traps.length; i++) {
-    trapIdx[traps[i]] = i;
-  }
-  d[start][0] = 0;
-  const queue = [[d[start][0], start, 0]];
-
-  let val, idx, state, nextState;
-  while (queue.length) {
-    distanceArr = queue.map((el) => el[0]);
-
-    const smallIdx = distanceArr.indexOf(Math.min(...distanceArr));
-
-    [val, idx, state] = queue[smallIdx];
-
-    queue.splice(smallIdx, 1);
-    if (idx === end) return val;
-
-    if (d[idx][state] !== val) continue;
-
-    for (const [next, dist] of adj[idx]) {
-      rev = 0;
-      if (trapIdx[idx] !== -1 && bitmask(state, idx)) rev ^= 1;
-      if (trapIdx[next] !== -1 && bitmask(state, next)) rev ^= 1;
-
-      if (rev) continue;
-      nextState = state;
-      if (trapIdx[next] !== -1) nextState ^= 1 << trapIdx[next];
-
-      if (d[next][nextState] > dist + val) {
-        d[next][nextState] = dist + val;
-        queue.push([d[next][nextState], next, nextState]);
-      }
-    }
-
-    for (const [next, dist] of adjRev[idx]) {
-      rev = 0;
-      if (trapIdx[idx] !== -1 && bitmask(state, idx)) rev ^= 1;
-      if (trapIdx[next] !== -1 && bitmask(state, next)) rev ^= 1;
-
-      if (!rev) continue;
-      nextState = state;
-      if (trapIdx[next] !== -1) nextState ^= 1 << trapIdx[next];
-
-      if (d[next][nextState] > dist + val) {
-        d[next][nextState] = dist + val;
-        queue.push([d[next][nextState], next, nextState]);
+  let q = []; // bfs를 위한 큐
+  for (let i = 0; i < rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+      if (maps[i][j] === "S") {
+        q = [[i, j, 0, 0]]; // row,col,이동 거리, 레버 당김 유무.
+        v[i][j][0] = true; // 방문도 체크
       }
     }
   }
 
+  while (q.length) {
+    const [r, c, cnt, isPulled] = q.shift();
+    for (const d of dirs) {
+      const nr = d[0] + r;
+      const nc = d[1] + c;
+      if (isValid(nr, nc) && maps[nr][nc] !== "X" && !v[nr][nc][isPulled]) {
+        v[nr][nc][isPulled] = true;
+        if (isPulled && maps[nr][nc] === "E") {
+          return cnt + 1;
+        } else if (maps[nr][nc] === "L") {
+          q.push([nr, nc, cnt + 1, 1]);
+        } else {
+          q.push([nr, nc, cnt + 1, isPulled]);
+        }
+      }
+    }
+  }
   return -1;
 }
-
-console.log(
-  solution(
-    3,
-    1,
-    3,
-    [
-      [1, 2, 2],
-      [3, 2, 3],
-    ],
-    [2]
-  )
-);
