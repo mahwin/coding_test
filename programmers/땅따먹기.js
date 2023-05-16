92843bd48ac8edf43d850e4762a7849f91da5cd8
youseock <mahwin7085@gmail.com>
2023-05-16 16:44:28 +0900

    프로그래머스 땅따먹기 : dp

--- programmers/땅따먹기.js
+++ programmers/땅따먹기.js
@@ -1,27 +1,22 @@
 function solution(land) {
-  var answer = 0;
-
-  let dp = Array.from({ length: land.length }, () => [0, 0, 0, 0]);
+  let len = land.length;
+  let canChoice = [
+    [1, 2, 3],
+    [0, 2, 3],
+    [0, 1, 3],
+    [0, 1, 2],
+  ];
+  const dp = Array.from({ length: len }, () =>
+    Array.from({ length: 4 }, () => 0)
+  );
   dp[0] = land[0];
 
-  let idxArr = [0, 1, 2, 3];
-  let idxObj = {};
-  idxArr.forEach((idx) => {
-    idxObj[idx] = idxArr.filter((i) => i !== idx);
-  });
-
-  for (let i = 1; i < land.length; i++) {
+  for (let i = 1; i < len; i++) {
     for (let j = 0; j < 4; j++) {
-      let values = dp[i - 1].filter((_, k) => k !== j);
-      dp[i][j] = Math.max(Math.max(...values) + land[i][j], dp[i - 1][j]);
+      const [a, b, c] = canChoice[j];
+      dp[i][j] =
+        Math.max(dp[i - 1][a], dp[i - 1][b], dp[i - 1][c]) + land[i][j];
     }
   }
-
-  return Math.max(...dp[dp.length - 1]);
+  return Math.max(...dp[len - 1]);
 }
-
-solution([
-  [1, 2, 3, 5],
-  [5, 6, 7, 8],
-  [4, 3, 2, 1],
-]);
