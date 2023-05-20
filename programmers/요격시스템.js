b11a60bbf7b07f541c61734da3cfc403653b5312
youseock <mahwin7085@gmail.com>
2023-05-20 22:37:52 +0900

    프로그래머스 요격 시스템 : 정렬

--- programmers/요격시스템.js
+++ programmers/요격시스템.js
@@ -1,11 +1,7 @@
 function solution(targets) {
-  let answer = 0;
+  targets.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
 
-  targets.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    else return a[1] - b[1];
-  });
-
+  let result = 0;
   let pre = 0;
 
   for (let i = 0; i < targets.length; i++) {
@@ -13,9 +9,9 @@ function solution(targets) {
     if (start < pre && end > pre) continue;
     else {
       pre = end - 0.5;
-      answer++;
+      result++;
     }
   }
 
-  return answer;
+  return result;
 }
