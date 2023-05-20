7221e3cbfcc52be39affaf7243fd51b624bb46e9
youseock <mahwin7085@gmail.com>
2023-05-20 22:40:33 +0900

    Add 다른 풀이

--- programmers/요격시스템.js
+++ programmers/요격시스템.js
@@ -15,3 +15,21 @@ function solution(targets) {
 
   return result;
 }
+
+function solution2(targets) {
+  targets.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+
+  let result = 0;
+  let pre = -1;
+
+  for (let i = 0; i < targets.length; i++) {
+    const [start, end] = targets[i];
+    if (pre < start) {
+      result++;
+      pre = end - 0.5;
+    } else {
+      pre = Math.min(end - 0.5, pre);
+    }
+  }
+  return result;
+}
