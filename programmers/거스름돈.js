b595e2f136bccf67b18565c901167768621d6425
youseock <mahwin7085@gmail.com>
2023-05-21 17:52:41 +0900

    프로그래머스 거스름 돈 : dp

--- programmers/거스름돈.js
+++ programmers/거스름돈.js
@@ -1,15 +1,17 @@
+const D = 1_000_000_007;
+
 function solution(n, money) {
-  const dp = Array.from({ length: n + 1 }).fill(0);
+  //dp[n]은 n원을 만들 수 있는 동전의 조합 수를 의미함.
+  const dp = Array.from({ length: n + 1 }, () => 0);
 
-  dp[0] = 1;
+  dp[0] = 1; // 코인과 거스름돈의 가격이 같을때, total-money ===0  1을 더 해주기 위해
 
-  for (let el of money) {
-    for (let i = el; i < n + 1; i++) {
-      dp[i] += dp[i - el];
+  for (const coin of money) {
+    for (let total = 1; total <= n; total++) {
+      if (total - coin >= 0) {
+        dp[total] += dp[total - coin] % D;
+      }
     }
   }
-  console.log(dp);
-  return dp[n] % 1000000007;
+  return dp[n];
 }
-
-solution(5, [1, 2, 5]);
