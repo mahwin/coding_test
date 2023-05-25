71f1cadb7e703b8c7322375f07209be40d575543
youseock <mahwin7085@gmail.com>
2023-05-25 17:15:47 +0900

    카카오 2023 표현 가능한 이진 트리 : 이진 트리 성질, 이진수

--- programmers/표현가능한이진트리.js
+++ programmers/표현가능한이진트리.js
@@ -1,35 +1,33 @@
-const isTree = (binary) => {
-  let center = (binary.length / 2) >> 0;
-  let parent = binary[center];
+const MAX = 10 ** 15;
+const MAX_BINARY_LEN = MAX.toString(2).length;
 
-  if (parent === "0") {
+const recur = (binary) => {
+  const mid = (binary.length / 2) >> 0;
+  const p = binary[mid];
+  if (p === "0") {
+    // 부모가 영이면 밑에 자식들은 다 영
     for (let i = 0; i < binary.length; i++) {
       if (binary[i] === "1") return false;
     }
   }
   if (binary.length === 1) return true;
-
-  return isTree(binary.slice(0, center)) && isTree(binary.slice(center + 1));
+  return recur(binary.slice(0, mid)) && recur(binary.slice(mid + 1));
 };
 
 function solution(numbers) {
   let answer = [];
   for (const num of numbers) {
     let binary = num.toString(2);
+    let binaryLen = binary.length;
     let binaryTreeLen = 0;
 
-    for (let i = 0; i < 51; i++) {
+    for (let i = 0; i <= MAX_BINARY_LEN; i++) {
       binaryTreeLen = 2 ** i - 1;
       if (binaryTreeLen >= binary.length) break;
     }
     let needLength = binaryTreeLen - binary.length;
-    if (needLength) binary = "0".padEnd(needLength, "0") + binary;
-
-    let isPosiible = isTree(binary);
-    if (isPosiible) answer.push(1);
-    else answer.push(0);
+    if (needLength) binary = "0".repeat(needLength) + binary;
+    recur(binary) ? answer.push(1) : answer.push(0);
   }
   return answer;
 }
-
-console.log(solution([4]));
