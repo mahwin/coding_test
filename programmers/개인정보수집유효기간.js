c0261e5280ec9cd53f158fcace18b32d4021d1ab
youseock <mahwin7085@gmail.com>
2023-05-26 11:12:10 +0900

    카카오 2023 개인정보 수집 유효 기간 : 구현, 문자열

--- programmers/개인정보수집유효기간.js
+++ programmers/개인정보수집유효기간.js
@@ -1,39 +1,23 @@
-const dateAddMonth = (date, month) => {
-  let [y, m, d] = date.split(".").map(Number);
-  let addY = Math.floor(month / 12);
-  y += addY;
-  let remainM = month - addY * 12;
-  if (m + remainM > 12) {
-    y++;
-    m = m + remainM - 12;
-  } else {
-    m += remainM;
-  }
+const lastDay = 28;
 
-  return [y, m, d];
+const dateToDay = (date) => {
+  const [y, m, d] = date.split(".").map(Number);
+  return y * lastDay * 12 + m * lastDay + d;
 };
 
 function solution(today, terms, privacies) {
-  let answer = [];
-  let termObj = {};
+  const termObj = {};
+  const result = [];
   terms.forEach((term) => {
-    const [t, m] = term.split(" ");
-    termObj[t] = Number(m);
+    const [type, duration] = term.split(" ");
+    termObj[type] = duration * lastDay;
   });
-  const [ty, tm, td] = today.split(".").map(Number);
+  const now = dateToDay(today);
 
-  for (let i = 0; i < privacies.length; i++) {
-    let dateAndType = privacies[i];
-    let [date, type] = dateAndType.split(" ");
-    const [y, m, d] = dateAddMonth(date, termObj[type]);
-    console.log(y, m, d);
-    if (ty > y) answer.push(i + 1);
-    else if (ty === y) {
-      if (tm > m) answer.push(i + 1);
-      else if (tm === m) {
-        if (td >= d) answer.push(i + 1);
-      }
-    }
-  }
-  return answer;
+  privacies.forEach((privacy, i) => {
+    const [date, type] = privacy.split(" ");
+    const finishedDay = dateToDay(date) + termObj[type];
+    if (finishedDay <= now) result.push(i + 1);
+  });
+  return result;
 }
