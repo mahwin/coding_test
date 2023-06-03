56f139ba6bad0a3e2eeda15b146a5fbb12d11350
youseock <mahwin7085@gmail.com>
2023-06-03 16:22:54 +0900

    프로그래머스 숫자 타자 대회 : dp

--- programmers/숫자타자대회.js
+++ programmers/숫자타자대회.js
@@ -1,143 +1,52 @@
-// const isValid = (x, y) => {
-//   if (y < 0 || y > 4 || x < 0 || x > 3) return false;
-//   else return true;
-// };
+// 거리  === 0  : 가중치 1
+// 거리  === 1  : 가중치 2
+// 1 < 거리 < 2 : 가중치 3
+// 거리  === 2  : 가중치 4
+// 3 > 거리 > 2 : 가중치 5
+// 4 > 거리 > 3 : 가중치 6
+// 거리  > 4    : 가중치 7
 
-// const bfs = (pos, target) => {
-//   let queue = [
-//     [...pos[0], 0, 1], //현 위치, 시행횟수, right of left
-//     [...pos[1], 0, 0],
-//   ];
-
-//   let dirs = [
-//     [1, 0],
-//     [0, 1],
-//     [-1, 0],
-//     [0, -1],
-//   ];
-//   let diagonals = [
-//     [1, -1],
-//     [1, 1],
-//     [-1, 1],
-//     [-1, -1],
-//   ];
-
-//   let cntBoard = Array.from({ length: 4 }, (_, i) =>
-//     Array.from({ length: 3 }, (_, j) => Infinity)
-//   );
-
-//   while (queue.length) {
-//     let [x, y, cnt, isLeft] = queue.shift();
-
-//     if (target[0] === x && target[1] === y) {
-//       if (cnt === 0) return [1, isLeft];
-//       else return [cnt, isLeft];
-//     }
-
-//     for (let dir of dirs) {
-//       let nx = x + dir[0];
-//       let ny = y + dir[1];
-//       if (isValid(nx, ny) && cntBoard[nx][ny] > cnt + 2) {
-//         cntBoard[nx][ny] = cnt + 2;
-//         queue.push([nx, ny, cnt + 2, isLeft]);
-//       }
-//     }
-//     for (let dia of diagonals) {
-//       let nx = x + dia[0];
-//       let ny = y + dia[1];
-//       if (isValid(nx, ny) && cntBoard[nx][ny] > cnt + 3) {
-//         cntBoard[nx][ny] = cnt + 3;
-//         queue.push([nx, ny, cnt + 3, isLeft]);
-//       }
-//     }
-//   }
-// };
-
-// const phoneNumToPos = () => {
-//   let phone = {};
-//   for (let i = 1; i < 10; i++) {
-//     let x = Math.floor((i - 1) / 3);
-//     let y = (i - 1) % 3;
-//     phone[i] = [x, y];
-//   }
-//   phone["0"] = [3, 1];
-//   return phone;
-// };
-
-// function solution(numbers) {
-//   let phoone = phoneNumToPos();
-
-//   let pos = [
-//     [1, 0],
-//     [1, 2],
-//   ];
-//   let total = 0;
-
-//   for (let i = 0; i < numbers.length; i++) {
-//     let num = numbers[i];
-//     const target = phoone[num];
-
-//     [cnt, isLeft] = bfs(pos, target);
-//     total += cnt;
-//     isLeft ? (pos[0] = target) : (pos[1] = target);
-//   }
-
-//   return total;
-// }
-
-const makeKey = (num1, num2) => {
-  return [num1, num2].join(",");
+const phone = {
+  1: [0, 0],
+  2: [0, 1],
+  3: [0, 2],
+  4: [1, 0],
+  5: [1, 1],
+  6: [1, 2],
+  7: [2, 0],
+  8: [2, 1],
+  9: [2, 2],
+  0: [3, 1],
 };
+
+const cal = (p1, p2) => {
+  const [x1, y1] = phone[p1];
+  const [x2, y2] = phone[p2];
+  const dist = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+
+  if (dist === 1) return 2;
+  else if (dist < 2) return 3;
+  else if (dist === 2) return 4;
+  else if (dist < 2.5) return 5;
+  else if (dist <= 3) return 6;
+  else return 7;
+};
+
 function solution(numbers) {
-  const costs = [
-    [1, 7, 6, 7, 5, 4, 5, 3, 2, 3],
-    [7, 1, 2, 4, 2, 3, 5, 4, 5, 6],
-    [6, 2, 1, 2, 3, 2, 3, 5, 4, 5],
-    [7, 4, 2, 1, 5, 3, 2, 6, 5, 4],
-    [5, 2, 3, 5, 1, 2, 4, 2, 3, 5],
-    [4, 3, 2, 3, 2, 1, 2, 3, 2, 3],
-    [5, 5, 3, 2, 4, 2, 1, 5, 3, 2],
-    [3, 4, 5, 6, 2, 3, 5, 1, 2, 4],
-    [2, 5, 4, 5, 3, 2, 3, 2, 1, 2],
-    [3, 6, 5, 4, 5, 3, 2, 4, 2, 1],
-  ];
-
-  let weightObj = {};
-  weightObj[makeKey(4, 6)] = 0;
-  let key;
-  for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i];
-    let tmpObj = {};
-    for (let [p, w] of Object.entries(weightObj)) {
-      let [l, r] = p.split(",");
-
-      if (l === num) {
-        key = makeKey(num, r);
-        if (!tmpObj[key] || tmpObj[key] > w + 1) {
-          tmpObj[key] = w + 1;
-        }
-      } else if (r === num) {
-        key = makeKey(l, num);
-        if (!tmpObj[key] || tmpObj[key] > w + 1) {
-          tmpObj[key] = w + 1;
-        }
+  let len = numbers.length;
+  const dp = Array.from({ length: len + 1 }, () => []);
+  dp[0] = [[4, 6, 0]]; // 왼, 오, 가중치 누적;
+  for (let i = 0; i < len; i++) {
+    dp[i].forEach(([l, r, cnt]) => {
+      const num = +numbers[i];
+      if (l == num || r == num) {
+        dp[i + 1].push([l, r, cnt + 1]);
       } else {
-        let lKey = makeKey(num, r);
-        let rKey = makeKey(l, num);
-
-        if (!tmpObj[rKey] || tmpObj[rKey] > w + costs[r][num]) {
-          tmpObj[rKey] = w + costs[r][num];
-        }
-        if (!tmpObj[lKey] || tmpObj[lKey] > w + costs[l][num]) {
-          tmpObj[lKey] = w + costs[l][num];
-        }
+        dp[i + 1].push([num, r, cnt + cal(l, num)]);
+        dp[i + 1].push([l, num, cnt + cal(r, num)]);
       }
-    }
-
-    weightObj = tmpObj;
+    });
   }
-
-  return Math.min(...Object.values(weightObj));
+  resultArr = dp[len].map((el) => el[2]);
+  return Math.min(...resultArr);
 }
-
-console.log(solution("1756"));
