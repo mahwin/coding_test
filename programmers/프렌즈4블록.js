0c86b504f4609dd0261036863eac07e2c20e5b49
youseock <mahwin7085@gmail.com>
2023-06-03 12:00:59 +0900

    카카오 2018 프렌즈4블록 : 문자열, 구현

--- programmers/프렌즈4블록.js
+++ programmers/프렌즈4블록.js
@@ -1,72 +1,65 @@
-function solution(m, n, board) {
-  let answer = 0;
-  const check = (row, col, board) => {
-    const indexArr = [
-      [row, col + 1],
-      [row + 1, col],
-      [row + 1, col + 1],
-    ];
+const dirs = [
+  [0, 1],
+  [1, 0],
+  [1, 1],
+]; //오 아래 대각
 
-    const current = board[row][col];
-    if (current === "X") return false;
+// const isValid = (r,c,m,n)=>{
+//     if(r>=m||c>=n|| r<0 || c<0) return false;
+//     else return true;
+// }
 
-    if (
-      current === board[row][col + 1] &&
-      current === board[row + 1][col] &&
-      current === board[row + 1][col + 1]
-    ) {
-      //2중 배열, 중복 비교 위해 스트링으로 변환
-
-      return [[row, col], ...indexArr].map((rowcol) => rowcol.join(","));
+//지워질 블록 찾기
+const find = (board, m, n) => {
+  let pos = [];
+  for (let r = 0; r < m - 1; r++) {
+    for (let c = 0; c < n - 1; c++) {
+      if (board[r][c] === ".") continue;
+      let samePos = [[r, c]];
+      for (const d of dirs) {
+        const nr = r + d[0];
+        const nc = c + d[1];
+        if (board[nr][nc] === board[r][c]) samePos.push([nr, nc]);
+        else break;
+      }
+      if (samePos.length === 4) pos.push(...samePos);
     }
-  };
-
-  const destroyBoard = (set) => {
-    const colObj = {};
-
-    [...set].forEach((rowcol) => {
-      const [row, col] = rowcol.split(",");
-      colObj[col] = colObj[col] ? [...colObj[col], row] : [row];
-    });
-
-    // colObj => col:[row1,row2]인데 오름차순으로 되어 있음.
-    Object.keys(colObj).forEach((col) => {
-      let deleteRows = colObj[col];
-      const colValues = [];
-      for (let row = 0; row < m; row++) {
-        colValues.push(board[row][col]);
-      }
-      deleteRows.forEach((row) => (colValues[row] = ""));
-      let newColvalues = colValues.join("").padStart(m, "X");
-
-      for (let row = 0; row < m; row++) {
-        board[row][col] = newColvalues[row];
-      }
-    });
-  };
-
-  board = board.map((row) => row.split(""));
-
-  let flag = true;
-  while (flag) {
-    flag = false;
-    let set = new Set();
-    for (let row = 0; row < m - 1; row++) {
-      for (let col = 0; col < n - 1; col++) {
-        let canBroke = check(row, col, board);
-        if (canBroke) {
-          flag = true;
-          canBroke.forEach((el) => set.add(el));
-        }
-      }
-    }
-    answer += set.size;
-    destroyBoard(set);
   }
+  return pos;
+};
 
-  return answer;
+const unique = (arr) => {
+  let set = new Set();
+  arr.forEach(([r, c]) => set.add([r, c].join(",")));
+  return [...set].map((el) => el.split(",").map(Number));
+};
+
+// 전략
+// 지울 부분에 .을 넣고 위에서부터 col 기준으로 아래 row부터 확인하고
+
+const down = (board, pos, m, n) => {
+  pos.forEach(([r, c]) => (board[r][c] = "."));
+  for (let c = 0; c < n; c++) {
+    let colInfo = "";
+    for (let r = 0; r < m; r++) {
+      if (board[r][c] !== ".") colInfo += board[r][c];
+    }
+    colInfo = colInfo.padStart(m, ".");
+    for (let r = 0; r < m; r++) {
+      board[r][c] = colInfo[r];
+    }
+  }
+};
+
+function solution(m, n, board) {
+  let cnt = 0;
+  board = board.map((el) => el.split(""));
+
+  while (true) {
+    const possible = find(board, m, n); // 지울 수 있는 배열 리턴하는 함수 호출
+    const uniquePos = unique(possible); // 중복 제외
+    if (uniquePos.length == 0) return cnt; // 지울 수 있는 배열이 없다면 리턴
+    cnt += uniquePos.length;
+    down(board, uniquePos, m, n); // 지울 수 있는 블록 지우고 위에 블록 아래로
+  }
 }
-
-console.log(
-  solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"])
-);
