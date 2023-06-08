499ebf4428d6e46dcae02bb50358d70c8c73c6d0
youseock <mahwin7085@gmail.com>
2023-06-08 11:40:44 +0900

    프로그래머스 마법의 엘리베이터 : 구현

--- programmers/마법의엘리베이터.js
+++ programmers/마법의엘리베이터.js
@@ -1,60 +1,23 @@
 function solution(storey) {
-  const ceilOrFloor = (num) => {
-    numArr = [...num.toString().split("").reverse().map(Number), 0];
+  let result = 0;
+  storey = storey.toString().split("").map(Number).reverse();
+  storey.push(0);
 
-    for (let i = 0; i < numArr.length - 1; i++) {
-      if (numArr[i] > 5) {
-        numArr[i + 1]++;
-        numArr[i] = 0;
-      } else if (numArr[i] === 5) {
-        if (numArr[i + 1] >= 5) {
-          numArr[i + 1]++;
-          numArr[i] = 0;
-        }
+  for (let i = 0; i < storey.length; i++) {
+    const num = storey[i];
+    if (num > 5) {
+      result += 10 - storey[i];
+      storey[i + 1]++;
+    } else if (num < 5) result += storey[i];
+    else {
+      if (storey[i + 1] >= 5) {
+        result += 10 - storey[i];
+        storey[i + 1]++;
+      } else {
+        result += storey[i];
       }
     }
+  }
 
-    const ceilNum = Number(numArr.reverse().join(""));
-
-    if (ceilNum === num) return countNum(num);
-    else return countNum(ceilNum - num) + countNum(ceilNum);
-  };
-
-  const countNum = (num) => {
-    let cnt = 0;
-
-    num
-      .toString()
-      .split("")
-      .forEach((n) => (cnt += +n));
-    return cnt;
-  };
-
-  return ceilOrFloor(storey);
+  return result;
 }
-
-// for (let num = 555; num < 600; num++) {
-//   console.log(solution(num), num);
-// }
-
-console.log(solution(16), 16);
-// 2554	16
-// 16 6
-
-// 6 -> 10
-
-// 56
-
-// 4 1 5번
-// 96 => 100 4
-
-// 595 11번이래
-// 1000 * 1
-// -100 * 4
-// -1 * 5
-
-// 595 1005
-// 6번
-
-// 95 6번
-// 35
