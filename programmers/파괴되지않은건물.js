e8cc04f5f1195f693bf1bb8317898c46d9bb9055
youseock <mahwin7085@gmail.com>
2023-06-09 14:03:24 +0900

    카카오 2022 파괴되지 않은 건물 : 2차원 누적합

--- programmers/파괴되지않은건물.js
+++ programmers/파괴되지않은건물.js
@@ -1,55 +1,36 @@
+//2차원 누적합
 function solution(board, skill) {
-  let answer = 0;
-
-  const accSumBoard = Array.from({ length: board.length + 1 }, () =>
-    Array.from({ length: board[0].length + 1 }, () => 0)
+  const [rowLen, colLen] = [board.length, board[0].length];
+  const accBoard = Array.from({ length: rowLen + 1 }, () =>
+    Array.from({ length: colLen + 1 }, () => 0)
   );
-
   skill.forEach(([type, r1, c1, r2, c2, degree]) => {
-    type === 1 ? (degree *= -1) : null;
-
-    accSumBoard[r1][c1] += degree;
-    accSumBoard[r1][c2 + 1] += -degree;
-    accSumBoard[r2 + 1][c1] += -degree;
-    accSumBoard[r2 + 1][c2 + 1] += degree;
+    const d = type === 1 ? -degree : degree;
+    accBoard[r1][c1] += d;
+    accBoard[r1][c2 + 1] -= d;
+    accBoard[r2 + 1][c1] -= d;
+    accBoard[r2 + 1][c2 + 1] += d;
   });
 
-  // 가로 방향 누적합
-  for (let row = 0; row < accSumBoard.length; row++) {
-    for (let col = 1; col < accSumBoard[0].length; col++) {
-      accSumBoard[row][col] += accSumBoard[row][col - 1];
+  // acc 행 방향 합
+  for (let r = 0; r < rowLen; r++) {
+    for (let c = 1; c < colLen; c++) {
+      accBoard[r][c] += accBoard[r][c - 1];
+    }
+  }
+  // acc 열 방향 합
+  for (let c = 0; c < colLen; c++) {
+    for (let r = 1; r < rowLen; r++) {
+      accBoard[r][c] += accBoard[r - 1][c];
+    }
+  }
+  // 스킬의 합과 건물 초기 내구도 비교
+  let result = 0;
+  for (let r = 0; r < rowLen; r++) {
+    for (let c = 0; c < colLen; c++) {
+      if (accBoard[r][c] + board[r][c] > 0) result++;
     }
   }
 
-  // 세로 방향 누적합
-  for (let row = 1; row < accSumBoard.length; row++) {
-    for (let col = 0; col < accSumBoard[0].length; col++) {
-      accSumBoard[row][col] += accSumBoard[row - 1][col];
-    }
-  }
-
-  board.forEach((rowInfo, rowIdx) =>
-    rowInfo.forEach((info, colIdx) => {
-      if (accSumBoard[rowIdx][colIdx] + info > 0) answer++;
-    })
-  );
-
-  return answer;
+  return result;
 }
-
-console.log(
-  solution(
-    [
-      [5, 5, 5, 5, 5],
-      [5, 5, 5, 5, 5],
-      [5, 5, 5, 5, 5],
-      [5, 5, 5, 5, 5],
-    ],
-    [
-      [1, 0, 0, 3, 4, 4],
-      [1, 2, 0, 2, 3, 2],
-      [2, 1, 0, 3, 1, 2],
-      [1, 0, 1, 3, 3, 1],
-    ]
-  )
-);
