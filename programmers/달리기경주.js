b36f37a14875f3543d9d8048eaff786eef529476
youseock <mahwin7085@gmail.com>
2023-06-15 08:44:55 +0900

    프로그래머스 달리기 경주 : map

--- programmers/달리기경주.js
+++ programmers/달리기경주.js
@@ -1,23 +1,20 @@
 function solution(players, callings) {
-  let playerObj = {};
-  let scoreObj = {};
+  const pGMap = new Map(); // 선수 이름 key : 등수 value
+  const gPMap = new Map(); // 등수 key : 선수 이름 value
   players.forEach((player, i) => {
-    playerObj[player] = i;
-    scoreObj[i] = player;
+    pGMap.set(player, i + 1);
+    gPMap.set(i + 1, player);
   });
-  callings.forEach((name) => {
-    const curIdx = playerObj[name];
-    const prePlayer = scoreObj[curIdx - 1];
-    playerObj[name]--;
-    playerObj[prePlayer]++;
-    scoreObj[curIdx] = prePlayer;
-    scoreObj[curIdx - 1] = name;
+
+  callings.forEach((player) => {
+    const g = pGMap.get(player);
+    const preP = gPMap.get(g - 1);
+
+    pGMap.set(player, g - 1);
+    pGMap.set(preP, g);
+    gPMap.set(g - 1, player);
+    gPMap.set(g, preP);
   });
-  const result = Array.from({ length: players.length }, () => null);
 
-  for (let [score, name] of Object.entries(scoreObj)) {
-    result[score] = name;
-  }
-
-  return result;
+  return [...gPMap.entries()].sort((a, b) => a[1] - b[1]).map((el) => el[1]);
 }
