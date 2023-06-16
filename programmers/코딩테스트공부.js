33dd02593e7c67c49639eff99c2d8087d1093411
youseock <mahwin7085@gmail.com>
2023-06-16 10:55:40 +0900

    카카오 2022 코딩 테스트 공부 : dp

--- programmers/코딩테스트공부.js
+++ programmers/코딩테스트공부.js
@@ -1,10 +1,10 @@
 function solution(alp, cop, problems) {
-  let [maxAlp, maxCop] = [alp, cop];
-
-  problems.forEach((problem) => {
-    maxAlp = Math.max(problem[0], maxAlp);
-    maxCop = Math.max(problem[1], maxCop);
-  });
+  let maxAlp = alp;
+  let maxCop = cop;
+  for (let i = 0; i < problems.length; i++) {
+    maxAlp = Math.max(maxAlp, problems[i][0]);
+    maxCop = Math.max(maxCop, problems[i][1]);
+  }
 
   const dp = Array.from({ length: maxAlp + 1 }, () =>
     Array.from({ length: maxCop + 1 }, () => Infinity)
@@ -13,28 +13,21 @@ function solution(alp, cop, problems) {
   dp[alp][cop] = 0;
 
   for (let i = alp; i <= maxAlp; i++) {
-    //알고력
-
     for (let j = cop; j <= maxCop; j++) {
-      //공부
-      if (i < maxAlp) {
+      if (i + 1 <= maxAlp)
         dp[i + 1][j] =
-          dp[i][j] + 1 > dp[i + 1][j] ? dp[i + 1][j] : dp[i][j] + 1;
-      }
-      if (j < maxCop) {
+          dp[i + 1][j] < dp[i][j] + 1 ? dp[i + 1][j] : dp[i][j] + 1;
+      if (j + 1 <= maxCop)
         dp[i][j + 1] =
-          dp[i][j + 1] > dp[i][j] + 1 ? dp[i][j] + 1 : dp[i][j + 1];
-      }
-
-      //문제이용
-
-      for (const [needA, needC, plusA, plusC, cost] of problems) {
-        if (needA <= i && needC <= j) {
-          const a = i + plusA > maxAlp ? maxAlp : i + plusA;
-
-          const c = j + plusC > maxAlp ? maxAlp : j + plusC;
-
-          dp[a][c] = dp[i][j] + cost > dp[a][c] ? dp[a][c] : dp[i][j] + cost;
+          dp[i][j + 1] < dp[i][j] + 1 ? dp[i][j + 1] : dp[i][j] + 1;
+      for (let [alp_req, cop_req, alp_rwd, cop_rwd, cost] of problems) {
+        if (i >= alp_req && j >= cop_req) {
+          const nAlpIdx = i + alp_rwd <= maxAlp ? i + alp_rwd : maxAlp;
+          const nCopIdx = j + cop_rwd <= maxCop ? j + cop_rwd : maxCop;
+          dp[nAlpIdx][nCopIdx] =
+            dp[nAlpIdx][nCopIdx] < dp[i][j] + cost
+              ? dp[nAlpIdx][nCopIdx]
+              : dp[i][j] + cost;
         }
       }
     }
@@ -42,12 +35,3 @@ function solution(alp, cop, problems) {
 
   return dp[maxAlp][maxCop];
 }
-
-console.log(
-  solution(0, 0, [
-    [0, 0, 2, 1, 2],
-    [4, 5, 3, 1, 2],
-    [4, 11, 4, 0, 2],
-    [10, 4, 0, 4, 2],
-  ])
-);
