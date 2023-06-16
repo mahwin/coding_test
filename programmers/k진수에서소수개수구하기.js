781100bdc21dbacbafee81e6cfce6d20db65af13
youseock <mahwin7085@gmail.com>
2023-06-16 09:32:51 +0900

    카카오 2022 k진수에세 소수 개수 구하기 : 진법,  구현

--- programmers/k진수에서소수개수구하기.js
+++ programmers/k진수에서소수개수구하기.js
@@ -1,32 +1,17 @@
-const transfer = (n, k) => {
-  if (k === 10) return "" + n;
-  let divs = [];
-  while (n) {
-    remainder = n % k;
-    divs.push(remainder);
-    n = Math.floor(n / k);
-  }
-  return divs.reverse().join("");
-};
-
 const isPrime = (num) => {
-  if (num <= 1) return false;
-  if (num === 2) return true;
-  for (let i = 3; i <= Math.ceil(Math.sqrt(num)); i++) {
+  num = Number(num);
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false;
   }
   return true;
 };
 
 function solution(n, k) {
-  let answer = 0;
-  let transferNum = transfer(n, k);
-
-  transferNum
-    .split("0")
-    .forEach((numStr) => (isPrime(+numStr) ? answer++ : null));
-
-  return answer;
+  const string = n.toString(k).split("0");
+  let result = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (isPrime(string[i])) result++;
+  }
+  return result;
 }
-
-solution(110011, 10);
