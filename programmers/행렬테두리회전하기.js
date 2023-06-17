4d2a9ba6b5f263a0b91e3b52392e955f842c14cc
youseock <mahwin7085@gmail.com>
2023-06-17 10:39:46 +0900

    프로그래머스 2021 dev-matching : 행렬 달팽이 회전

--- programmers/행렬테두리회전하기.js
+++ programmers/행렬테두리회전하기.js
@@ -1,53 +1,39 @@
-const zeroPadding = (board) => {
-  let tmp = Array.from({ length: board[0].length + 2 }, () => 0);
-  board = board.map((b) => [0, ...b, 0]);
-  return [tmp, ...board, tmp];
-};
-
 function solution(rows, columns, queries) {
-  let answer = [];
-  let board = Array.from({ length: rows }, (_, i) =>
-    Array.from({ length: columns }, (_, j) => i * columns + j + 1)
+  let result = [];
+
+  const board = Array.from({ length: rows }, (_, r) =>
+    Array.from({ length: columns }, (_, c) => r * columns + c + 1)
   );
-  board = zeroPadding(board);
 
-  const boardRotator = (r1, c1, r2, c2) => {
-    const values = [];
-    const indexArr = [];
-
-    for (let col = c1; col <= c2; col++) {
-      indexArr.push([r1, col]);
-      values.push(board[r1][col]);
+  queries.forEach((el) => {
+    const [sR, sC, eR, eC] = el.map((num) => num - 1);
+    console.log(sR, sC, eR, eC);
+    let tmp = [];
+    let pos = [];
+    for (let c = sC; c <= eC; c++) {
+      tmp.push(board[sR][c]);
+      pos.push([sR, c]);
+    }
+    for (let r = sR + 1; r <= eR; r++) {
+      tmp.push(board[r][eC]);
+      pos.push([r, eC]);
+    }
+    for (let c = eC - 1; c >= sC; c--) {
+      tmp.push(board[eR][c]);
+      pos.push([eR, c]);
+    }
+    for (let r = eR - 1; r >= sR; r--) {
+      tmp.push(board[r][sC]);
+      pos.push([r, sC]);
     }
 
-    for (let row = r1 + 1; row <= r2; row++) {
-      indexArr.push([row, c2]);
-      values.push(board[row][c2]);
-    }
+    result.push(Math.min(...tmp));
+    const last = tmp.pop();
+    tmp.unshift(last);
 
-    for (let col = c2 - 1; col >= c1; col--) {
-      indexArr.push([r2, col]);
-      values.push(board[r2][col]);
-    }
-
-    for (let row = r2 - 1; row > r1; row--) {
-      indexArr.push([row, c1]);
-      values.push(board[row][c1]);
-    }
-    let i = indexArr.shift();
-    indexArr.push(i);
-    indexArr.forEach(([row, col], i) => {
-      board[row][col] = values[i];
+    pos.forEach(([r, c], i) => {
+      board[r][c] = tmp[i];
     });
-
-    return Math.min(...values);
-  };
-
-  queries.forEach(([r1, c1, r2, c2]) => {
-    answer.push(boardRotator(r1, c1, r2, c2));
   });
-
-  return answer;
+  return result;
 }
-
-solution(6, 6, [[2, 2, 5, 4]]);
