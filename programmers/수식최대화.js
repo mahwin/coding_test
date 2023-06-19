99d5b6b2573763788a21af23e27ec973b91c2051
youseock <mahwin7085@gmail.com>
2023-06-19 12:00:57 +0900

    카카오 2020 수식최대화 : dfs, 구현

--- programmers/수식최대화.js
+++ programmers/수식최대화.js
@@ -1,66 +1,55 @@
-const expParser = (expression) => {
-  const result = [];
-  let stack = "";
-  for (let i = 0; i < expression.length; i++) {
-    const cur = expression[i];
-    //지금 값이 연산자에 포함되면 stack에 저장했던 값과 현재 연사자를 배열에 저장하고
-    //stack은 초기화한다.
-    if (["+", "-", "*"].includes(cur)) {
-      result.push(stack);
-      result.push(cur);
-      stack = "";
-    } else stack += cur;
+const OPERERATOR = ["+", "-", "*"];
+const parser = (string) => {
+  const arr = [];
+
+  let tmp = "";
+  for (let i = 0; i < string.length; i++) {
+    const cur = string[i];
+    if (cur.match(/[\+\-\*]/)) {
+      arr.push(Number(tmp));
+      arr.push(cur);
+      tmp = "";
+    } else tmp += cur;
   }
-  //stack에 남아있는 숫자 포함
-  result.push(stack);
-  return result;
+  arr.push(Number(tmp));
+  return arr;
 };
 
-//백트래킹 돌면서 연사자를 뽑을 순서르 정한다.
-const dfs = (v, permutations, opers) => {
-  if (opers.length === 3) {
-    permutations.push(opers);
-    return;
-  }
-  for (let i = 0; i < 3; i++) {
-    if (v[i]) continue;
-    v[i] = true;
-    dfs(v, permutations, opers.concat(i));
-    v[i] = false;
-  }
-};
+const cal = (opers, arr) => {
+  let copy = [...arr];
 
-const calAbsolute = (expArr, per) => {
-  const opers = "+-*";
-  for (let i = 0; i < 3; i++) {
-    const pickOper = opers[per[i]];
-    let tmp = [];
-    for (let j = 0; j < expArr.length; j++) {
-      if (expArr[j] === pickOper) {
-        tmp.push(eval(tmp.pop() + pickOper + expArr[j + 1]));
-        j++;
-      } else tmp.push(expArr[j]);
+  for (let oper of opers) {
+    for (let i = 0; i < copy.length; i++) {
+      if (copy[i] == oper) {
+        const tmp = eval(`${copy[i - 1]} ${copy[i]} ${copy[i + 1]}`);
+        copy = [...copy.slice(0, i - 1), tmp, ...copy.slice(i + 2)];
+        i -= 2;
+      }
     }
-    expArr = tmp;
   }
-
-  return Math.abs(expArr[0]);
+  return Math.abs(copy[0]);
 };
 
 function solution(expression) {
-  let result = 0;
+  let result = -Infinity;
+  const splitArr = parser(expression);
 
-  const expArr = expParser(expression);
+  // dfs 돌면서 순열 생성
   const v = Array.from({ length: 3 }, () => false);
-  const permutations = [];
+  const dfs = (tmp) => {
+    if (tmp.length == 3) {
+      result = Math.max(cal(tmp, splitArr), result);
+      return;
+    }
+    for (let i = 0; i < 3; i++) {
+      if (v[i]) continue;
+      v[i] = true;
+      dfs(tmp.concat(OPERERATOR[i]));
+      v[i] = false;
+    }
+  };
 
-  dfs(v, permutations, []); // permutations 배열에 순열 채우기
-
-  for (const per of permutations) {
-    result = Math.max(result, calAbsolute(expArr, per));
-  }
+  dfs([]);
 
   return result;
 }
-
-console.log(solution("100-200*300-500+20"));
