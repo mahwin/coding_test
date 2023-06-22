f1163083fbaf479048ec54c3700d5bbe959b5360
youseock <mahwin7085@gmail.com>
2023-06-22 23:42:24 +0900

    백준 7490 0 만들기 : bfs, 구현

--- backjoon/0만들기.js
+++ backjoon/0만들기.js
@@ -1,52 +1,62 @@
-let input = `2
+let input = `4
 3
-7`.split("\n");
+7
+5
+6`.split("\n");
+
+// const fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const operators = ["+", " ", "-"];
+let tmp = [];
+let result = [];
+
+const cal = (len) => {
+  let sic = [];
+  for (let i = 1; i <= len; i++) {
+    sic.push(i);
+    sic.push(tmp[i - 1]);
+  }
+  sic.pop();
+
+  let key = sic.join("");
+  let score = eval(key.replaceAll(" ", ""));
+  if (score == 0) result.push(key);
+};
+
+const dfs = (node, len) => {
+  if (node === len - 1) {
+    cal(len);
+    return;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    tmp.push(operators[i]);
+    dfs(node + 1, len);
+    tmp.pop();
+  }
+};
 
 const solution = () => {
-  const [n, ...arr] = input.map(Number);
-  const max = Math.max(...arr);
-  const result = {};
-  for (let key of arr) {
-    result[key] = new Set();
+  const map = new Map();
+  const n = Number(input.shift());
+  for (let i = 0; i < n; i++) {
+    const len = Number(input[i]);
+
+    if (map.has(len)) continue;
+    else {
+      dfs(0, len);
+      map.set(len, result.sort().join("\n"));
+      result = [];
+    }
   }
-
-  const opers = [];
-
-  const check = () => {
-    let concatString = "1";
-    for (let i = 2; i <= opers.length; i++) {
-      concatString += opers[i - 1] + i;
-    }
-    return eval(concatString.split(" ").join("")) === 0 ? concatString : null;
-  };
-
-  const dfs = () => {
-    if (arr.includes(opers.length)) {
-      const key = opers.length;
-      const concatString = check();
-      if (concatString) {
-        result[key] = result[key]
-          ? result[key].add(concatString)
-          : new Set(concatString);
-      }
-    }
-    if (opers.length >= max) return;
-
-    for (let oper of [" ", "+", "-"]) {
-      opers.push(oper);
-      dfs();
-      opers.pop();
-    }
-  };
-  dfs();
-  let print = "";
-  for (let key of arr) {
-    for (const o of result[key]) {
-      print += o + "\n";
-    }
-    print += "\n";
+  let answer = "";
+  for (let i = 0; i < n; i++) {
+    const len = Number(input[i]);
+    answer += map.get(len) + "\n";
+    answer += "\n";
   }
-  console.log(print.trim());
+  console.log(answer.trim());
 };
 
 solution();
