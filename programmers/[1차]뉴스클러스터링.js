45eafd65a4acd621f1624180e11a7919d896fc2d
youseock <mahwin7085@gmail.com>
2023-06-23 12:42:39 +0900

    카카오 2018 [1차] 뉴스 클러스터링 : set,map

--- programmers/[1차]뉴스클러스터링.js
+++ programmers/[1차]뉴스클러스터링.js
@@ -1,34 +1,49 @@
-const convert = (str) => {
-  const cntObj = {};
-  for (let i = 0; i < str.length - 1; i++) {
-    if (str[i].match(/[a-z]/) && str[i + 1].match(/[a-z]/)) {
-      const key = str.slice(i, i + 2);
-      cntObj[key] = cntObj[key] ? cntObj[key] + 1 : 1;
+const countObj = (arr) => {
+  const obj = {};
+  arr.forEach((num) => {
+    obj[num] = obj[num] ? obj[num] + 1 : 1;
+  });
+  return obj;
+};
+const findUnionSet = (arr1, arr2) => {
+  let set = new Set();
+
+  arr1.forEach((num) => set.add(num));
+  arr2.forEach((num) => set.add(num));
+  return [...set];
+};
+const splitForJakard = (string) => {
+  const stringArr = [];
+  let a = 0;
+  for (let i = 0; i < string.length - 1; i++) {
+    const [a, b] = string
+      .slice(i, i + 2)
+      .toLowerCase()
+      .split("");
+    if (a.match(/[a-z]/) && b.match(/[a-z]/)) {
+      //둘 다 알파벳이면
+      stringArr.push([a, b].join(""));
     }
   }
-  return cntObj;
+  return stringArr;
 };
 
 function solution(str1, str2) {
-  [str1, str2] = [str1, str2].map((el) => el.toLowerCase());
-  const cntObj1 = convert(str1);
-  const cntObj2 = convert(str2);
-  const unionSet = new Set();
-  Object.keys(cntObj1).forEach((key) => unionSet.add(key));
-  Object.keys(cntObj2).forEach((key) => unionSet.add(key));
-  // 합집합의 갯수
-  console.log(unionSet);
-  let unionNum = 0;
-  [...unionSet].forEach((key) => {
-    unionNum += Math.max(cntObj1[key] || 0, cntObj2[key] || 0);
-  });
-  // 교집합의 개수
-  let interNum = 0;
-  [...unionSet].forEach((key) => {
-    if (cntObj1[key] && cntObj2[key]) {
-      interNum += Math.min(cntObj1[key], cntObj2[key]);
-    }
-  });
+  const str1Arr = splitForJakard(str1);
+  const str2Arr = splitForJakard(str2);
+  const obj1 = countObj(str1Arr);
+  const obj2 = countObj(str2Arr);
+  let union = findUnionSet(str1Arr, str2Arr);
 
-  return unionNum > 0 ? Math.floor((interNum / unionNum) * 65536) : 65536;
+  let uniCnt = 0;
+  let interCnt = 0;
+  for (const key of union) {
+    if (obj1[key] && obj2[key]) {
+      interCnt += Math.min(obj1[key], obj2[key]);
+    }
+    uniCnt += Math.max(obj1[key] || 0, obj2[key] || 0);
+  }
+
+  if (uniCnt == 0 && interCnt == 0) return 65536;
+  else return Math.floor((interCnt / uniCnt) * 65536);
 }
