9a3cb422487279722a4380152d2df9effed8ab03
youseock <mahwin7085@gmail.com>
2023-06-26 19:24:10 +0900

    프로그래머스 공원 산책 : bfs

--- programmers/공원산책.js
+++ programmers/공원산책.js
@@ -1,46 +1,41 @@
-let board = [];
-let rowLen, colLen, robot;
+let rowLen, colLen;
+const dirObj = { E: [0, 1], S: [1, 0], N: [-1, 0], W: [0, -1] };
+
+const isValid = (r, c, nr, nc, park) => {
+  if (nr < 0 || nc < 0 || nr >= rowLen || nc >= colLen) return false;
+  const [minR, maxR] = [Math.min(r, nr), Math.max(r, nr)];
+  const [minC, maxC] = [Math.min(c, nc), Math.max(c, nc)];
+
+  for (let row = minR; row <= maxR; row++) {
+    for (let col = minC; col <= maxC; col++) {
+      if (park[row][col] === "X") return false;
+    }
+  }
+  return true;
+};
+
+const findInit = (park) => {
+  for (let r = 0; r < rowLen; r++) {
+    for (let c = 0; c < colLen; c++) {
+      if (park[r][c] == "S") return [r, c];
+    }
+  }
+};
 
 function solution(park, routes) {
   rowLen = park.length;
   colLen = park[0].length;
 
-  // 초기값 찾기
-  for (let r = 0; r < rowLen; r++) {
-    if (robot) break;
-    for (let c = 0; c < colLen; c++) {
-      if (park[r][c] === "S") {
-        robot = [r, c];
-        break;
-      }
+  let [r, c] = findInit(park);
+
+  routes.forEach((el) => {
+    const [d, len] = el.split(" ");
+    const nr = r + Number(len) * dirObj[d][0];
+    const nc = c + Number(len) * dirObj[d][1];
+
+    if (isValid(r, c, nr, nc, park)) {
+      [r, c] = [nr, nc];
     }
-  }
-  routes.forEach((route) => move(park, route));
-
-  return robot;
+  });
+  return [r, c];
 }
-
-const isValid = (r, c) => {
-  if (r < 0 || c < 0 || r >= rowLen || c >= colLen) return false;
-  return true;
-};
-
-const dirs = { N: [-1, 0], S: [1, 0], W: [0, -1], E: [0, 1] };
-
-const move = (park, route) => {
-  //N S W E
-  let [d, distance] = route.split(" ");
-  const dir = dirs[d];
-  distance = Number(distance);
-  let [pr, pc] = robot;
-  for (let i = 0; i < distance; i++) {
-    let nr = pr + dir[0];
-    let nc = pc + dir[1];
-    if (!isValid(nr, nc) || park[nr][nc] === "X") return;
-    pr = nr;
-    pc = nc;
-  }
-  robot = [pr, pc];
-};
-
-console.log(solution(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]));
