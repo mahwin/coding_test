021582e4df49b46b7112530f622f0012ea436d97
youseock <mahwin7085@gmail.com>
2023-06-27 21:34:03 +0900

    카카오 2022 등산코스 정하기 : queue, 그래프 탐색
    - 인접 리스트 만들 때 concat 해서 새로운 배열 만드는 것보다 기존 배열에 push하는 편이 훨씬 빠름!!!!!!

--- programmers/등산코스정하기.js
+++ programmers/등산코스정하기.js
@@ -1,94 +1,140 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+// 정확성 O, 효율성 X
+let result = [0, Infinity];
+const graph = {};
+const summitObj = {};
+let gateObj = {};
+let v;
+const dfs = (node, intensity) => {
+  if (summitObj[node]) {
+    if (intensity < result[1]) {
+      result = [node, intensity];
+    } else if (intensity == result[1] && node < result[0]) {
+      result[0] = node;
+    }
+    return;
   }
+  if (!graph[node]) return;
+
+  for (const [next, nextInten] of graph[node]) {
+    if (v[next]) continue;
+    v[next] = true;
+    dfs(next, Math.max(intensity, nextInten));
+    v[next] = false;
+  }
+};
+function solution1(n, paths, gates, summits) {
+  v = Array.from({ length: n + 1 }, () => false);
+  summits.forEach((summit) => (summitObj[summit] = true));
+  gates.forEach((gate) => (v[gate] = true));
+
+  paths.forEach(([from, to, intensity]) => {
+    //양방향
+    graph[from] = graph[from]
+      ? graph[from].concat([[to, intensity]])
+      : [[to, intensity]];
+    graph[to] = graph[to]
+      ? graph[to].concat([[from, intensity]])
+      : [[from, intensity]];
+  });
+
+  gates.forEach((start) => {
+    dfs(start, 0);
+  });
+
+  return result;
 }
+
+// queue 추가
 
 class Queue {
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(val) {
+    this.queue[this.rear++] = val;
+  }
+
+  dequeue() {
+    const val = this.queue[this.front];
+    delete this.queue[this.front++];
+    return val;
   }
 
   isEmpty() {
-    return this.size === 0 ? true : false;
-  }
-
-  enqueue(data) {
-    const node = new Node(data);
-    if (!this.head) this.head = node;
-    else this.tail.next = node;
-
-    this.tail = node;
-    this.size++;
-  }
-  dequeue() {
-    if (!this.head) return null;
-    const data = this.head.data;
-    this.head = this.head.next;
-    this.size--;
-    return data;
+    return this.front === this.rear;
   }
 }
 
 function solution(n, paths, gates, summits) {
-  let graph = Array.from({ length: n + 1 }, () => []); //그래프
-  const queue = new Queue(); // 큐
-  const d = Array.from({ length: n + 1 }, () => Infinity); // 다익스크라 값 배열
+  const q = new Queue();
+  const graph = {};
+  const summitObj = {};
+  const gateObj = {};
+  const dp = Array.from({ length: n + 1 }, () => 10000001);
+  dp[0] = 0;
 
-  //양방향 그래프
-  paths.forEach(([from, to, intensity]) => {
-    graph[from].push([to, intensity]);
-    graph[to].push([from, intensity]);
+  summits.forEach((summit) => (summitObj[summit] = true));
+  gates.forEach((gate) => {
+    q.enqueue({ node: gate, intensity: 0 }); // gate 번호와 intensity 0
+    gateObj[gate] = true;
+    dp[gate] = 0;
   });
 
-  //우선 순위 큐 첫 시작 값
-  for (let i = 0; i < gates.length; i++) {
-    queue.enqueue([gates[i], 0]);
-  }
+  paths.forEach(([from, to, intensity]) => {
+    //양방향
+    graph[from] = graph[from]
+      ? graph[from].concat([[to, intensity]])
+      : [[to, intensity]];
+    graph[to] = graph[to]
+      ? graph[to].concat([[from, intensity]])
+      : [[from, intensity]];
+  });
 
-  while (!queue.isEmpty()) {
-    const [node, intensity] = queue.dequeue();
+  while (!q.isEmpty()) {
+    const { node: current, intensity: currIntensity } = q.dequeue();
+    if (summitObj[current] || !graph[current]) continue;
 
-    if (d[node] <= intensity) continue;
-    d[node] = intensity;
-    if (summits.includes(node)) continue;
-
-    for (let [next, nextIntens] of graph[node]) {
-      const nextIntensity = intensity > nextIntens ? intensity : nextIntens;
-
-      if (d[next] <= nextIntensity) continue;
-      // d[next] = nextIntensity;
-      queue.enqueue([next, nextIntensity]);
+    for (const [next, nextIntensity] of graph[current]) {
+      const maxIntensity =
+        currIntensity > nextIntensity ? currIntensity : nextIntensity;
+      if (maxIntensity < dp[next]) {
+        dp[next] = maxIntensity;
+        q.enqueue({ node: next, intensity: maxIntensity });
+      }
     }
   }
-  let answer = [0, Infinity];
+  // let result = [0, Infinity];
+  // for (const summit of summits) {
+  //   const intensity = dp[summit];
+  //   if (result[1] < intensity) continue;
+  //   if (result[1] > intensity) result = [summit, intensity];
+  //   else if (result[0] > summit) result[0] = summit;
+  // }
+  // return result;
 
-  for (const summit of summits) {
-    if (d[summit] < answer[1]) {
-      answer = [summit, d[summit]];
-    } else if (d[summit] === answer[1] && summit < answer[0]) {
-      answer = [summit, d[summit]];
-    }
-  }
-
-  return answer;
+  return dp
+    .filter((v, i) => summitObj[i])
+    .map((v, i) => [summits[i], v])
+    .sort((a, b) => a[1] - b[1])
+    .filter((v, _, arr) => v[1] === arr[0][1])
+    .sort((a, b) => a[0] - b[0])[0];
 }
 
-solution(
-  6,
-  [
-    [1, 2, 3],
-    [2, 3, 5],
-    [2, 4, 2],
-    [2, 5, 4],
-    [3, 4, 4],
-    [4, 5, 3],
-    [4, 6, 1],
-    [5, 6, 1],
-  ],
-  [1, 3],
-  [5]
+console.log(
+  solution(
+    5,
+    [
+      [1, 3, 10],
+      [1, 4, 20],
+      [2, 3, 4],
+      [2, 4, 6],
+      [3, 5, 20],
+      [4, 5, 6],
+    ],
+    [1, 2],
+    [5]
+  )
 );
