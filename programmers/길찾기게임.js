9e830207d33b348614744341a42f315cd896c984
youseock <mahwin7085@gmail.com>
2023-06-27 19:11:34 +0900

    카카오 2019 길 찾기 게임 : 이진 탐색 트리, 전위 순회, 후위 순회

--- programmers/길찾기게임.js
+++ programmers/길찾기게임.js
@@ -1,8 +1,8 @@
 class Node {
-  constructor(x, y, index) {
+  constructor(idx, x, y) {
+    this.idx = idx;
     this.x = x;
     this.y = y;
-    this.index = index;
     this.left = null;
     this.right = null;
   }
@@ -14,19 +14,23 @@ class BinaryTree {
   }
 
   push(node) {
-    if (this.root === null) {
+    // 루트 노드가 없으면 루트 노드로 설정
+    if (!this.root) {
       this.root = node;
       return;
     }
+
     if (this.root.x > node.x) {
-      if (this.root.left === null) {
+      //루트 x가 현재 노드의 x보다 더 크다면 현재 노드를 왼쪽에 배치
+      if (!this.root.left) {
         this.root.left = new BinaryTree(node);
         return;
       } else {
         this.root.left.push(node);
       }
     } else {
-      if (this.root.right === null) {
+      //루트 x가 현재 노드의 x보다 더 작다면 현재 노드를 오른쪽에 배치
+      if (!this.root.right) {
         this.root.right = new BinaryTree(node);
         return;
       } else {
@@ -34,43 +38,46 @@ class BinaryTree {
       }
     }
   }
-  preorder(tmp) {
-    tmp.push(this.root.index);
-    if (this.root.left) this.root.left.preorder(tmp);
-    if (this.root.right) this.root.right.preorder(tmp);
-  }
-
-  postorder(tmp) {
-    if (this.root.left) this.root.left.postorder(tmp);
-    if (this.root.right) this.root.right.postorder(tmp);
-    tmp.push(this.root.index);
-  }
 }
 
-function solution(nodeinfo) {
-  nodeinfo = nodeinfo.map((el, index) => [...el, index + 1]); //인덱스 추가
+const getPre = (pre, tree) => {
+  pre.push(tree.root.idx);
+  if (tree.root.left) getPre(pre, tree.root.left);
+  if (tree.root.right) getPre(pre, tree.root.right);
+};
 
-  nodeinfo.sort((a, b) => b[1] - a[1]); // y값이 클 수록 루트 노드에 근접
+const getPost = (post, tree) => {
+  if (tree.root.left) getPost(post, tree.root.left);
+  if (tree.root.right) getPost(post, tree.root.right);
+  post.push(tree.root.idx);
+};
 
-  const binaryTree = new BinaryTree();
-  for (const [x, y, index] of nodeinfo) {
-    const node = new Node(x, y, index);
-    binaryTree.push(node);
+const solution = (nodeInfo) => {
+  let tree = new BinaryTree();
+
+  nodeInfo = nodeInfo.map((el, i) => [i + 1, el[0], el[1]]); // 정렬하기 전에 node의 인덱스 추가
+  nodeInfo.sort((a, b) => b[2] - a[2]); // y 좌표 내림 차순으로 정렬
+
+  for (let i = 0; i < nodeInfo.length; i++) {
+    tree.push(new Node(...nodeInfo[i]));
   }
-  let [pre, post] = [[], []];
-  binaryTree.preorder(pre);
-  binaryTree.postorder(post);
+  let pre = [];
+  let post = [];
+  getPre(pre, tree);
+  getPost(post, tree);
   return [pre, post];
-}
+};
 
-solution([
-  [5, 3],
-  [11, 5],
-  [13, 3],
-  [3, 5],
-  [6, 1],
-  [1, 3],
-  [8, 6],
-  [7, 2],
-  [2, 2],
-]);
+console.log(
+  solution([
+    [5, 3],
+    [11, 5],
+    [13, 3],
+    [3, 5],
+    [6, 1],
+    [1, 3],
+    [8, 6],
+    [7, 2],
+    [2, 2],
+  ])
+);
