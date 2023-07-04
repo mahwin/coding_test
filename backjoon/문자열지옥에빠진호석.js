02018b1a29a26205c50a4e2fc4fa9ec3c392952a
youseock <mahwin7085@gmail.com>
2023-07-04 15:38:47 +0900

    백준 20166 문자열 지옥에 빠진 호석 : dfs, map, 백트래킹

--- backjoon/문자열지옥에빠진호석.js
+++ backjoon/문자열지옥에빠진호석.js
@@ -1,51 +1,62 @@
+let input = `3 4 3
+abcb
+bcaa
+abac
+aba
+abc
+cab`.split("\n");
 // const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let input = `3 3 2
-aaa
-aba
-aaa
-aa
-bb`.split("\n");
-function solution() {
-  const [n, m, k] = input.shift().split(" ").map(Number);
+let rowLen, colLen, k;
+const dictionaryObj = new Map();
+const board = [];
+const dirs = [
+  //상하좌우
+  [0, 1],
+  [1, 0],
+  [-1, 0],
+  [0, -1],
+  //대각선
+  [-1, -1],
+  [1, 1],
+  [-1, 1],
+  [1, -1],
+];
 
-  const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-    [1, 1],
-    [1, -1],
-    [-1, 1],
-    [-1, -1],
-  ];
-  const dictionaryObj = new Map();
+const dfs = (r, c, string) => {
+  dictionaryObj.set(string, (dictionaryObj.get(string) || 0) + 1);
 
-  const dfs = (row, col, tmp) => {
-    dictionaryObj.set(tmp, (dictionaryObj.get(tmp) || 0) + 1); // 현재 키 값에 해당하는 값이 없으면 0
+  if (string.length == 5) return;
 
-    if (tmp.length == 5) return; //종료조건 신이 좋아하는 문자열의 길이 1~5
+  for (const d of dirs) {
+    const nr = (d[0] + r + rowLen) % rowLen;
+    const nc = (d[1] + c + colLen) % colLen;
+    dfs(nr, nc, string + board[nr][nc]);
+  }
+};
 
-    for (let i = 0; i < 8; i++) {
-      //환형 좌표계 표현. 음수값 처리하기 위해 + len해서 양수로 바꿔줌.
-      const nr = (row + dirs[i][0] + n) % n;
-      const nc = (col + dirs[i][1] + m) % m;
-      // 끝이 처음과 연결되어 있어서 경계 체크 x
-      dfs(nr, nc, tmp + input[nr][nc]);
-    }
-  };
-
-  for (let row = 0; row < n; row++) {
-    for (let col = 0; col < m; col++) {
-      dfs(row, col, input[row][col]);
+const fillObj = () => {
+  for (let r = 0; r < rowLen; r++) {
+    for (let c = 0; c < colLen; c++) {
+      dfs(r, c, board[r][c]);
     }
   }
+};
+
+const solution = () => {
+  [rowLen, colLen, k] = input.shift().split(" ").map(Number);
+
+  for (let i = 0; i < rowLen; i++) {
+    board.push(input[i]);
+  }
+  fillObj();
+
   let result = [];
-  for (let i = 0; i < k; i++) {
-    result.push(dictionaryObj.get(input[n + i]) || 0);
+  for (let i = rowLen; i < rowLen + k; i++) {
+    result.push(dictionaryObj.get(input[i]) || 0);
   }
   console.log(result.join("\n"));
-}
+};
 
 solution();
