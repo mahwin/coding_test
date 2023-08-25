1455d3f0eafecb1782b3093a46bafa153771b882
youseock <mahwin7085@gmail.com>
2023-08-25 22:13:57 +0900

    백준 2357 최솟값과 최댓값 : 세그먼트 트리

---
+++ backjoon/최솟값과 최댓값.js
@@ -0,0 +1,104 @@
+let input = `10 4
+75
+30
+100
+38
+50
+51
+52
+20
+81
+5
+1 10
+3 5
+6 9
+8 10`.split("\n");
+
+const fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+class SegmentTree {
+  constructor(nums) {
+    this.nums = nums;
+    this.depth = this.getDepth();
+    this.tree = Array.from({ length: 2 ** (this.depth + 1) }, () => [
+      Infinity,
+      -Infinity,
+    ]); // 세그먼트 트리 배열
+    this.init();
+  }
+
+  getDepth() {
+    let k = 1;
+    while (2 ** k < this.nums.length) {
+      k++;
+    }
+    return k;
+  }
+
+  init() {
+    let leftIdx = this.converTreeIdx(0);
+    let rightIdx = this.converTreeIdx(this.nums.length);
+    for (let i = 0; i < this.nums.length; i++) {
+      this.tree[i + leftIdx + 1] = [this.nums[i], this.nums[i]];
+    }
+
+    for (let i = rightIdx; i >= 1; i--) {
+      let pIdx = Math.floor(i / 2);
+      while (pIdx >= 1) {
+        this.tree[pIdx] = this.merge(this.tree[pIdx], this.tree[i]);
+        pIdx = Math.floor(pIdx / 2);
+      }
+    }
+  }
+
+  merge(leftValue, rightValue) {
+    const [leftMin, leftMax] = leftValue;
+    const [rightMin, rightMax] = rightValue;
+    return [Math.min(leftMin, rightMin), Math.max(leftMax, rightMax)];
+  }
+
+  converTreeIdx(idx) {
+    return idx + 2 ** this.depth - 1;
+  }
+
+  queryRange(start, end) {
+    this.minMax = [Infinity, -Infinity];
+    const leftIdx = this.converTreeIdx(start);
+    const rightIdx = this.converTreeIdx(end);
+    this.search(leftIdx, rightIdx);
+    return this.minMax;
+  }
+  search(leftIdx, rightIdx) {
+    if (leftIdx > rightIdx) return;
+    if (leftIdx % 2 === 1) {
+      if (this.tree[leftIdx]) {
+        this.minMax = this.merge(this.minMax, this.tree[leftIdx]);
+      }
+    }
+    if (rightIdx % 2 === 0) {
+      if (this.tree[rightIdx]) {
+        // console.log(this.tree[rightIdx]);
+        this.minMax = this.merge(this.minMax, this.tree[rightIdx]);
+      }
+    }
+
+    this.search(Math.floor((leftIdx + 1) / 2), Math.floor((rightIdx - 1) / 2));
+  }
+}
+
+function solution() {
+  let [N, M] = input[0].split(" ").map(Number);
+  const arr = input.slice(1, N + 1).map(Number);
+
+  const segTree = new SegmentTree(arr);
+  let result = [];
+  for (let qIdx = N + 1; qIdx <= N + M; qIdx++) {
+    result.push(
+      segTree.queryRange(...input[qIdx].split(" ").map(Number)).join(" ")
+    );
+  }
+  console.log(result.join("\n"));
+}
+
+solution();
