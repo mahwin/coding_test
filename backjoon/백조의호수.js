f99de990d62be90737b8fad39fdbf22113f9e990
youseock <mahwin7085@gmail.com>
2023-09-25 18:05:52 +0900

    백준 3197 백조의 호수 : bfs

--- backjoon/백조의호수.js
+++ backjoon/백조의호수.js
@@ -1,86 +1,116 @@
-let input = `1 7
-LXX.XXL`.split("\n");
+let input = `8 17
+...XXXXXX..XX.XXX
+....XXXXXXXXX.XXX
+...XXXXXXXXXXXX..
+..XXXXX.LXXXXXX..
+.XXXXXX..XXXXXX..
+XXXXXXX...XXXX...
+..XXXXX...XXX....
+....XXXXX.XXXL...`.split("\n");
 
-// const fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
-const findBird = (n, m, lake) => {
-  for (let r = 0; r < n; r++) {
-    for (let c = 0; c < m; c++) {
-      if (lake[r][c] == "L") {
-        lake[r][c] = ".";
-        return [r, c];
-      }
-    }
-  }
-};
-const dirs = [
-  [0, 1],
-  [1, 0],
-  [-1, 0],
-  [0, -1],
-];
-
-const isValid = (n, m, r, c) => {
-  if (r < 0 || c < 0 || r >= n || c >= m) return false;
-  else return true;
-};
-
-const melt = (n, m, lake) => {
-  const melts = [];
-  for (let r = 0; r < n; r++) {
-    for (let c = 0; c < m; c++) {
-      if (lake[r][c] === "X") {
-        for (const d of dirs) {
-          const nr = r + d[0];
-          const nc = c + d[1];
-
-          if (isValid(n, m, nr, nc) && lake[nr][nc] !== "X") {
-            melts.push([r, c]);
-            break;
-          }
-        }
-      }
-    }
-  }
-  melts.forEach(([r, c]) => (lake[r][c] = "."));
-};
-
-const isMeet = (initPos, n, m, lake) => {
-  const v = Array.from({ length: n }, () =>
-    Array.from({ length: m }, () => false)
-  );
-  v[initPos[0]][initPos[1]] = true;
-  const queue = [[...initPos]];
-
-  while (queue.length) {
-    const [r, c] = queue.shift();
-    if (lake[r][c] == "L") return true;
-    for (const d of dirs) {
-      const nr = r + d[0];
-      const nc = c + d[1];
-      if (isValid(n, m, nr, nc) && !v[nr][nc] && lake[nr][nc] !== "X") {
-        v[nr][nc] = true;
-        queue.push([nr, nc]);
-      }
-    }
-  }
-
-  return false;
-};
-
-function sol(n, m, lake) {
-  lake = lake.map((el) => el.split(""));
-  let bird = findBird(n, m, lake);
-  let day = 1;
-  const melts = melt(n, m, lake);
-  while (true) {
-    melt(n, m, lake);
-    if (isMeet(bird, n, m, lake)) return day;
-    day++;
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
   }
 }
 
-const [n, m] = input.shift().split(" ").map(Number);
-const lake = input;
-console.log(sol(n, m, lake));
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(item) {
+    const node = new Node(item);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.size += 1;
+  }
+
+  dequeue() {
+    const popItem = this.head;
+    this.head = this.head.next;
+    this.size -= 1;
+    return popItem.item;
+  }
+}
+
+const solution = () => {
+  const dirs = [
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [0, -1],
+  ];
+  const [rowLen, colLen] = input.shift().split(" ").map(Number);
+  const 백조 = [];
+  const board = [];
+  const Wqueue = new Queue();
+  for (let i = 0; i < rowLen; i++) {
+    const lakeInfo = input[i].split("");
+    for (let j = 0; j < colLen; j++) {
+      if (lakeInfo[j] === "L") {
+        lakeInfo[j] = ".";
+        백조.push([i, j]);
+        Wqueue.enqueue([i, j]);
+      } else if (lakeInfo[j] == ".") Wqueue.enqueue([i, j]);
+    }
+    board.push(lakeInfo);
+  }
+
+  const isValid = (r, c) => {
+    if (r >= rowLen || c >= colLen || c < 0 || r < 0) return false;
+    return true;
+  };
+  const isNextLake = (Wqueue) => {
+    let len = Wqueue.size;
+    for (let i = 0; i < len; i++) {
+      const [r, c] = Wqueue.dequeue();
+      for (const d of dirs) {
+        const nr = d[0] + r;
+        const nc = d[1] + c;
+        if (isValid(nr, nc) && board[nr][nc] === "X") {
+          board[nr][nc] = ".";
+          Wqueue.enqueue([nr, nc]);
+        }
+      }
+    }
+  };
+
+  const isCanReach = (board, 백조) => {
+    const q = new Queue();
+    q.enqueue(백조[0]);
+    const v = Array.from({ length: rowLen }, () =>
+      Array.from({ length: colLen }, () => false)
+    );
+
+    while (q.size !== 0) {
+      const [r, c] = q.dequeue();
+      if (r === 백조[1][0] && c === 백조[1][1]) return true;
+
+      for (const d of dirs) {
+        const nr = r + d[0];
+        const nc = c + d[1];
+        if (isValid(nr, nc) && board[nr][nc] == "." && !v[nr][nc]) {
+          v[nr][nc] = true;
+          q.enqueue([nr, nc]);
+        }
+      }
+    }
+    return false;
+  };
+  let day = 1;
+  while (1) {
+    isNextLake(Wqueue);
+    if (isCanReach(board, 백조)) return day;
+    day++;
+  }
+};
+
+console.log(solution());
