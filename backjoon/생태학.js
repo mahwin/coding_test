5c35a01fcac91d9cd9a78766a0d88a462dc4d7ec
youseock <mahwin7085@gmail.com>
2023-10-05 23:30:28 +0900

    백준 4358 생태계 : 구현

---
+++ backjoon/생태학.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let input = `Red Alder
+Ash
+Aspen
+Basswood
+Ash
+Beech
+Yellow Birch
+Ash
+Cherry
+Cottonwood
+Ash
+Cypress
+Red Elm
+Gum
+Hackberry
+White Oak
+Hickory
+Pecan
+Hard Maple
+White Oak
+Soft Maple
+Red Oak
+Red Oak
+White Oak
+Poplan
+Sassafras
+Sycamore
+Black Walnut
+Willow`.split("\n");
+
+const solution = () => {
+  let 종 = {};
+  let total = input.length * 0.01;
+  input.forEach((name) => {
+    if (종[name]) {
+      종[name]++;
+    } else {
+      종[name] = 1;
+    }
+  });
+  let result = [];
+  const names = Object.keys(종).sort();
+
+  names.forEach((name) => {
+    result.push(`${name} ${(종[name] / total).toFixed(4)}`);
+  });
+  console.log(result.join("\n"));
+};
+
+solution();
