570048d4d358acb8af86c74d60beb7c7a1da319e
youseock <mahwin7085@gmail.com>
2023-10-14 22:10:02 +0900

    백준 20546 기적의매매법 : 구현

---
+++ backjoon/기적의매매법🐜.js
@@ -0,0 +1,58 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const solution = () => {
+  let money = Number(input[0]);
+  let prices = input[1].split(" ").map(Number);
+
+  let acounts = [
+    [money, 0],
+    [money, 0],
+  ];
+
+  let s = 0;
+  for (let i = 0; i < prices.length; i++) {
+    const price = prices[i];
+    //buy and pray
+    let [oneMoney, oneCnt] = acounts[0];
+    const canbuy = Math.floor(oneMoney / price);
+    acounts[0] = [oneMoney - canbuy * price, oneCnt + canbuy];
+
+    //33
+    let [twoMoney, twoCnt] = acounts[1];
+
+    if (i === 0 || s === 0) {
+      if (prices[i - 1] > prices[i]) s = -1;
+      else if (prices[i - 1] < prices[i]) s = 1;
+      else s = 0;
+    } else {
+      if (s > 0 && prices[i - 1] < prices[i]) {
+        s++;
+      } else if (s < 0 && prices[i - 1] > prices[i]) {
+        s--;
+      } else {
+        if (prices[i - 1] > prices[i]) s = -1;
+        else if (prices[i - 1] < prices[i]) s = 1;
+        else s = 0;
+      }
+    }
+
+    if (s === 3) {
+      acounts[1] = [twoMoney + price * twoCnt, 0];
+    }
+    if (s <= -3) {
+      const canbuy = Math.floor(twoMoney / price);
+
+      acounts[1] = [twoMoney - canbuy * price, twoCnt + canbuy];
+    }
+  }
+
+  let result = [];
+  for (const [m, c] of acounts) {
+    result.push(m + c * prices[13]);
+  }
+
+  if (result[0] > result[1]) console.log("BNP");
+  else if (result[0] < result[1]) console.log("TIMING");
+  else console.log("SAMESAME");
+};
