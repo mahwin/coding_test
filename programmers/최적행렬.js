c3a83e58dc9adba1168615337e9dd4b120d8c99e
youseock <mahwin7085@gmail.com>
2023-11-07 23:11:57 +0900

    프로그래머스 최적 행렬
    - 행렬 연산

---
+++ programmers/최적행렬.js
@@ -0,0 +1,25 @@
+function solution(m) {
+  const { length } = m;
+  const dp = Array.from({ length: length + 1 }, () =>
+    Array.from({ length: length + 1 }, () => 0)
+  );
+
+  for (let n = 1; n <= length; n++) {
+    for (let i = 0; i < length - n; i++) {
+      let init = i;
+      let last = n + i;
+      for (let k = init; k < last; k++) {
+        if (dp[init][last] === 0) {
+          dp[init][last] =
+            dp[init][k] + dp[k + 1][last] + m[init][0] * m[k][1] * m[last][1];
+        }
+        dp[init][last] = Math.min(
+          dp[init][last],
+          dp[init][k] + dp[k + 1][last] + m[init][0] * m[k][1] * m[last][1]
+        );
+      }
+    }
+  }
+
+  return dp[0][length - 1];
+}
