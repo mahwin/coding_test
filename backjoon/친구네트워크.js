21305b630197bdf65a7f21765c1b27a35c80283e
youseock <mahwin7085@gmail.com>
2023-12-29 16:05:36 +0900

    백준 4195 친구 네트워크 : 유니온 파인드

--- backjoon/친구네트워크.js
+++ backjoon/친구네트워크.js
@@ -1,82 +1,65 @@
-// const readline = require("readline");
+let input = `2
+5
+a b
+b c
+a d
+a e
+e a
+`.split("\n");
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// let input = [];
-
-// rl.on("line", (line) => {
-//   input.push(line);
-// });
-
-// rl.on("close", () => {
-//   solution();
-//   console.log(result.trim());
-//   process.exit();
-// });
-
-const fs = require("fs");
-let input = fs.readFileSync("./data//data.txt").toString().split("\n");
-
-let friend = []; // [nameIdx, 연결된 수]
-let nameIdxMap = new Map();
-let result = "";
+const result = [];
 
 const solution = () => {
-  const n = input[0];
+  const testCases = +input[0];
+  let idx = 1;
+  for (let i = 0; i < testCases; i++) {
+    const n = +input[idx];
+    cal(input.slice(idx + 1, idx + n + 1));
+    idx += n + 1;
+  }
+  console.log(result.join("\n"));
+};
 
-  let inputIdx = 0;
-  for (let i = 0; i < n; i++) {
-    inputIdx += 1;
-    const k = input[inputIdx];
-    init(k);
+const cal = (input) => {
+  const friendObj = new Map();
 
-    for (let j = 0; j < k; j++) {
-      inputIdx++;
-
-      const [nameA, nameB] = input[inputIdx].split(" ");
-      const aIdx = getNameIndex(nameA);
-      const bIdx = getNameIndex(nameB);
-      union(aIdx, bIdx);
+  input = input.map((el) => {
+    const [name1, name2] = el.split(" ");
+    if (!friendObj.has(name1)) {
+      friendObj.set(name1, friendObj.size);
     }
+    if (!friendObj.has(name2)) {
+      friendObj.set(name2, friendObj.size);
+    }
+    return [friendObj.get(name1), friendObj.get(name2)];
+  });
+
+  const p = Array.from({ length: friendObj.size + 1 }, (_, i) => [i, 1]);
+
+  input.forEach(([a, b]) => {
+    union(a, b, p);
+    result.push(getP(a, p)[1]);
+  });
+};
+
+const getP = (x, p) => {
+  if (p[x][0] === x) return p[x];
+  p[x] = getP(p[x][0], p);
+  return p[x];
+};
+
+const union = (a, b, p) => {
+  const ap = getP(a, p);
+  const bp = getP(b, p);
+
+  if (ap[0] === bp[0]) return;
+
+  if (ap[0] > bp[0]) {
+    p[a][0] = bp[0];
+    p[b][1] += ap[1];
+  } else {
+    p[b][0] = ap[0];
+    p[a][1] += bp[1];
   }
 };
-
-const getNameIndex = (name) => {
-  if (!nameIdxMap.has(name)) nameIdxMap.set(name, nameIdxMap.size);
-  return nameIdxMap.get(name);
-};
-
-const getP = (idx) => {
-  if (idx === friend[idx][0]) return idx;
-  else {
-    friend[idx][0] = getP(friend[idx][0]);
-    return friend[idx][0];
-  }
-};
-
-const union = (aIdx, bIdx) => {
-  const aP = getP(aIdx);
-  const bP = getP(bIdx);
-  if (aP !== bP) {
-    if (aP < bP) {
-      friend[bP][0] = [aP];
-      friend[aP][1] += friend[bP][1];
-      result += friend[aP][1] + "\n";
-    } else {
-      friend[aP][0] = [pP];
-      friend[bP][1] += friend[aP][1];
-      result += friend[bP][1] + "\n";
-    }
-  } else result += friend[aP][1] + "\n";
-};
-
-const init = (k) => {
-  //초기화
-  friend = Array.from({ length: k * 2 }, (_, i) => [i, 1]); //연결된 최상위 노드, 연결된 수 !
-  nameIdxMap = new Map();
-};
-
 solution();
-console.log(result.trim());
