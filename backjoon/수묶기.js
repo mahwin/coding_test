b050e7a8dd702191829fd47a0c4f205823ceead7
youseock <mahwin7085@gmail.com>
2023-12-30 11:56:51 +0900

    백준 1744 수묶기 : 그리디

--- backjoon/수묶기.js
+++ backjoon/수묶기.js
@@ -1,32 +1,49 @@
-let input = `2
-1
-1`.split("\n");
+let input = `10
+-1
+-2
+-3
+-4
+-5
+-6
+-7
+-8
+-9
+-10`.split("\n");
 
-const n = Number(input[0]);
-const minus = [];
-const plus = [];
-let result = 0;
-for (let i = 1; i <= n; i++) {
-  num = Number(input[i]);
-  if (num === 1) result++;
-  else if (num > 0) plus.push(num);
-  else minus.push(num);
-}
-plus.sort((a, b) => b - a);
-minus.sort((a, b) => a - b);
+const solution = () => {
+  input.shift();
+  input = input.map(Number).sort((a, b) => a - b);
 
-for (let i = 0; i < plus.length; i++) {
-  if (i + 1 < plus.length) {
-    result += plus[i] * plus[i + 1];
-    i += 1;
-  } else result += plus[i];
-}
+  const minusArr = [];
+  let plusArr = [];
+  input.forEach((el) => {
+    if (el > 0) plusArr.push(el);
+    else minusArr.push(el);
+  });
+  plusArr = plusArr.reverse();
+  let result = 0;
+  for (let i = 0; i < minusArr.length; i++) {
+    if (i + 1 < minusArr.length) {
+      const [a, b] = [minusArr[i], minusArr[i + 1]];
+      i++;
+      result += Math.max(a * b, a + b);
+      console.log(a, b, result);
+    } else {
+      result += minusArr[i];
+      console.log(minusArr[i], result);
+    }
+  }
 
-for (let i = 0; i < minus.length; i++) {
-  if (i + 1 < minus.length) {
-    result += minus[i] * minus[i + 1];
-    i += 1;
-  } else result += minus[i];
-}
+  for (let i = 0; i < plusArr.length; i++) {
+    if (i + 1 < plusArr.length) {
+      const [a, b] = [plusArr[i], plusArr[i + 1]];
+      i++;
+      result += Math.max(a * b, a + b);
+    } else {
+      result += plusArr[i];
+    }
+  }
+  console.log(result);
+};
 
-console.log(result);
+solution();
