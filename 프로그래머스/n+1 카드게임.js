220b11e8d6a9e5297288a8ddc2cfeb7d4ffa80a0
youseock <mahwin7085@gmail.com>
2024-07-27 21:30:31 +0900

    프로그래머스 n+1 카드게임 : 그리디 수정

--- 프로그래머스/n+1 카드게임.js
+++ 프로그래머스/n+1 카드게임.js
@@ -1,86 +1,84 @@
-// 어떤 조합으로 낼지, 어떤 카드를 선택할 지 등등...
-// 완전 탐색이고 dfs 문제인 것 같음.
-
-function deleteElbyIndexs(arr, ...ids) {
-  return arr.filter((_, i) => ids.indexOf(i) === -1);
-}
+// 기존 카드의 조합으로 N+1을 만들거나
+// coin으로 새로운 카드를 하나 얻고, 기존 카드에서 하나 뽑거나
+// coin으로 새로운 카드를 두개 얻어서 N+1을 만드는 경우가 존재한다.
+// coin으로 카드를 뽑을지 말지는 선택할 경우를 나중으로 미뤄도 된다는 점을 이해하고 문제를 들어가자.
+// 각각의 경우를 3개의 배열에 담자.
+// 기존 카드에서 버리는 경우를 최우선으로 coin1, coin2 배열로 찾되 남은 코인을 확인하면서 스테이지를 진행하자.
 
 function solution(coin, cards) {
-  var answer = 0;
+  const LEN = cards.length;
+  const TARGET_NUM = LEN + 1;
+  const pass = { origin: 0, coin1: 0, coin2: 0 };
 
-  const lastCardIdx = cards.length;
-  const startCardIdx = lastCardIdx / 3;
-  const targetNumber = lastCardIdx + 1;
+  let originSet = new Set();
+  let coinSet = new Set();
 
-  let newCards;
-
-  let result = 0;
-  dfs(cards.slice(0, startCardIdx), coin, 1, startCardIdx);
-
-  function dfs(currentCards, coin, round, cardIdx) {
-    result = Math.max(round, result);
-    if (cardIdx >= lastCardIdx) return;
-
-    const len = currentCards.length;
-    const pickOne = cards[cardIdx];
-    const pickTwo = cards[cardIdx + 1];
-
-    for (let i = 0; i < len; i++) {
-      for (let j = i + 1; j < len; j++) {
-        if (currentCards[i] + currentCards[j] === targetNumber) {
-          dfs(
-            deleteElbyIndexs(currentCards, i, j),
-            coin,
-            round + 1,
-            cardIdx + 2
-          );
-          if (coin >= 2) {
-            dfs(
-              deleteElbyIndexs(currentCards, i, j).concat(pickOne, pickTwo),
-              coin - 2,
-              round + 1,
-              cardIdx + 2
-            );
-          }
-        }
-      }
-    }
-
-    if (coin >= 2 && pickOne + pickTwo === targetNumber) {
-      dfs(currentCards, coin - 2, round + 1, cardIdx + 2);
-    }
-
-    if (coin >= 1) {
-      for (let i = 0; i < len; i++) {
-        if (currentCards[i] + pickOne === targetNumber) {
-          dfs(
-            deleteElbyIndexs(currentCards, i),
-            coin - 1,
-            round + 1,
-            cardIdx + 2
-          );
-          if (coin >= 2) {
-            let newArr = deleteElbyIndexs(currentCards, i);
-            newArr.push(pickOne);
-            dfs(newArr, coin - 2, round + 1, cardIdx + 2);
-          }
-        }
-
-        if (currentCards[i] + pickTwo === targetNumber) {
-          dfs(
-            deleteElbyIndexs(currentCards, i),
-            coin - 1,
-            round + 1,
-            cardIdx + 2
-          );
-          if (coin >= 2) {
-            let newArr = deleteElbyIndexs(currentCards, i);
-            newArr.push(pickOne);
-            dfs(newArr, coin - 2, round + 1, cardIdx + 2);
-          }
-        }
-      }
-    }
+  for (let i = 0; i < LEN / 3; i++) {
+    const card = cards[i];
+    if (originSet.has(TARGET_NUM - card)) {
+      pass.origin++;
+      originSet.delete(TARGET_NUM - card);
+    } else originSet.add(card);
   }
-  return result;
+
+  let stage = 0;
+
+  for (let i = LEN / 3; i <= LEN; i += 2) {
+    stage++;
+
+    const card1 = cards[i];
+    const card2 = cards[i + 1];
+    const pair1 = TARGET_NUM - card1;
+    const pair2 = TARGET_NUM - card2;
+
+    coinSet.add(card1);
+    coinSet.add(card2);
+
+    if (card1 + card2 === TARGET_NUM) {
+      pass.coin2++;
+    }
+
+    if (coinSet.has(pair1)) {
+      coinSet.delete(pair1);
+      pass.coin2++;
+    }
+
+    if (originSet.has(pair1)) {
+      originSet.delete(pair1);
+      pass.coin1++;
+    }
+
+    if (coinSet.has(pair2)) {
+      coinSet.delete(pair2);
+      pass.coin2++;
+    }
+
+    if (originSet.has(pair2)) {
+      originSet.delete(pair2);
+      pass.coin1++;
+    }
+
+    if (pass.origin > 0) {
+      pass.origin--;
+      continue;
+    }
+
+    if (coin === 0) return stage;
+
+    if (coin > 0 && pass.coin1 > 0) {
+      pass.coin1--;
+      coin--;
+      continue;
+    }
+
+    if (coin > 1 && pass.coin2 > 0) {
+      pass.coin2--;
+      coin -= 2;
+      continue;
+    }
+
+    break;
+  }
+
+  return stage;
 }
