59117ca99e3ea352993672c989ebad032cf97669
youseock <mahwin7085@gmail.com>
2024-07-28 17:31:17 +0900

    프로그래머스 두 큐의 합 같게 만들기 : 그리디, queue 모사해서 해결

--- 프로그래머스/두큐합같게만들기.js
+++ 프로그래머스/두큐합같게만들기.js
@@ -1,104 +1,45 @@
 function sumArr(arr) {
-  return arr.reduce((a, c) => c + a, 0);
+  return arr.reduce((a, c) => (a += c));
 }
 
-function checkCanDivied(arr1, arr2) {
-  const totalSum = sumArr(arr1) + sumArr(arr2);
-  return totalSum % 2 === 0;
+function canDivideTwo(num1, num2) {
+  return (num1 + num2) % 2 === 0;
 }
+
+// [a,b,c,d], [e,f,g,h]
+// [a,b,c,d,e,f,g,h] , [] // n번
+// [h] [a,b,c,d,e,f,g] 2n -1 번
+// 총 3n번 옮기면 전체 경우를 다 확인할 수 있다.
 
 function solution(queue1, queue2) {
-  // 모든 요소의 합을 2로 나눌 수 없다면 두 개의 큐에 나눠 담을 수 없다.
-  if (!checkCanDivied(queue1, queue2)) return -1;
+  const len = queue1.length;
+  let sumQ1 = sumArr(queue1);
+  let sumQ2 = sumArr(queue2);
 
-  // 주의에 오버플로우 발생할 수 있다고 하니 BigInt를 사용하자.
-  let queue1Sum = BigInt(sumArr(queue1));
-  const totalSum = queue1Sum + BigInt(sumArr(queue2));
-  const targetSum = totalSum / 2n;
+  if (!canDivideTwo(sumQ1, sumQ2)) return -1;
 
-  // 시도 횟수가 얼마를 넘어가면 불가능 하다고 판단 내려야할까
-  // worst를 고려해보자
+  const target = (sumQ1 + sumQ2) / 2;
 
-  // [a,b,c] [d,e,f]
-  // ...
-  // [f] [a,b,c,d,e] => worst
-  // queue2를 queue1으로 모두 옮긴다 n
-  // 2n크기의 queue1을 하나만 남기고 모두 queue2로 옮긴다. 2n-1
+  let q1 = 0;
+  let q2 = 0;
 
-  // 총 3n - 1
-
-  const maxTrial = 3 * queue1.length + 1;
   let trial = 0;
+  let max_trials = len * 3 + 1;
 
-  // 아래 코드는 시간초과로 76점을 받음.
-  // queue1 = queue1.map(BigInt);
-  // queue2 = queue2.map(BigInt);
-  // while(trial < maxTrial){
-  //     if(queue1Sum === targetSum) return trial;
-  //     else if(queue1Sum > targetSum ){
-  //         const first = queue1.shift();
-  //         queue2.push(first);
-  //         queue1Sum-=first;
-  //     }
-  //     else {
-  //         const first = queue2.shift();
-  //         queue1.push(first);
-  //         queue1Sum += first;
-  //     }
-  //     trial++;
-  // }
+  while (max_trials--) {
+    if (sumQ1 === target) return trial;
 
-  // Queue를 구현하자
-
-  const q1 = new Queue();
-  const q2 = new Queue();
-  queue1.forEach((num) => q1.insert(BigInt(num)));
-  queue2.forEach((num) => q2.insert(BigInt(num)));
-
-  while (trial < maxTrial) {
-    if (queue1Sum === targetSum) return trial;
-    else if (queue1Sum > targetSum) {
-      const first = q1.pop();
-      q2.insert(first);
-      queue1Sum -= first;
+    if (sumQ1 > target) {
+      const popNum = queue1[q1++];
+      sumQ1 -= popNum;
+      queue2.push(popNum);
     } else {
-      const first = q2.pop();
-      q1.insert(first);
-      queue1Sum += first;
+      const popNum = queue2[q2++];
+      sumQ1 += popNum;
+      queue1.push(popNum);
     }
     trial++;
   }
 
   return -1;
-}
-
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  insert(data) {
-    const node = new Node(data);
-    if (!this.head) this.head = node;
-    else this.tail.next = node;
-
-    this.tail = node;
-    this.size++;
-  }
-  pop() {
-    if (!this.head) return null;
-    const data = this.head.data;
-    this.head = this.head.next;
-    this.size--;
-    return data;
-  }
 }
